100c19fd15fc6850d8e3c9f097ebb148278d7ffe
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2020-09-09 21:32:04 +0100

    Add a bit more the altitude FCU controls.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,37 +23,6 @@ using System.Runtime.CompilerServices;
 
 namespace FSInputMapper
 {
-    public class FSIMModel
-    {
-        public string text { get; set; }
-    }
-
-    public class FSIMViewModel : INotifyPropertyChanged
-    {
-        private FSIMModel model = new FSIMModel { text = "-----", };
-
-        public string Text
-        {
-            get { return model.text; }
-            set
-            {
-                if (model.text != value)
-                {
-                    model.text = value;
-                    OnPropertyChange();
-                }
-            }
-        }
-
-        //TODO: is there not a superclass or something to do this stuff for us?
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChange([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
     public partial class MainWindow : Window
     {
         const int WM_USER_SIMCONNECT = 0x0402;
@@ -65,30 +34,36 @@ namespace FSInputMapper
         {
             InitializeComponent();
             DataContext = _viewModel = new FSIMViewModel();
-
-            try
-            {
-                //simconnect = new SimConnect("Managed Datazmo", new WindowInteropHelper(this).EnsureHandle(), WM_USER_SIMCONNECT, null, 0);
-            }
-            catch (COMException ex)
-            {
-                MessageBox.Show("Boo hiss " + ex);
-            }
-            //if (simconnect != null) { simconnect.Dispose(); simconnect = null; }
         }
 
-        private void TestTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void Push_Click(object sender, RoutedEventArgs e)
         {
+            _viewModel.Altitude = 10000;
         }
 
-        private void Push_Clicked(object sender, RoutedEventArgs e)
+        private void Pull_Click(object sender, RoutedEventArgs e)
         {
-            _viewModel.Text = "M";
+            _viewModel.Altitude = 66600;
         }
 
-        private void Pull_Clicked(object sender, RoutedEventArgs e)
+        private void Up1000_Click(object sender, RoutedEventArgs e)
         {
-            _viewModel.Text = "S";
+            _viewModel.Altitude += 1000;
+        }
+
+        private void Up100_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Altitude += 100;
+        }
+
+        private void Down1000_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Altitude -= 1000;
+        }
+
+        private void Down100_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Altitude -= 100;
         }
     }
 }
