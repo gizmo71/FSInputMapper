aa0583a7a8130e64b19b35fabfc23ec53ebe547d
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2020-09-15 20:33:01 +0100

    Shorten enum names.

--- SimConnectAdapter.cs
+++ SimConnectAdapter.cs
@@ -6,10 +6,10 @@ using Microsoft.FlightSimulator.SimConnect;
 
 namespace FSInputMapper
 {
-    enum DATA_DEFINITIONS { AUTOPILOT_DATA = 69, SPOILER_DATA, SPOILER_HANDLE, }
-    enum REQUESTS { AUTOPILOT_DATA = 71, MORE_SPOILER, LESS_SPOILER, }
-    enum EVENTS { NONE = 42, DISARM_SPOILER, ARM_SPOILER, MORE_SPOILER, LESS_SPOILER, }
-    enum GROUPS { SPOILERS = 13, }
+    enum DATA { AUTOPILOT_DATA = 69, SPOILER_DATA, SPOILER_HANDLE, }
+    enum REQUEST { AUTOPILOT_DATA = 71, MORE_SPOILER, LESS_SPOILER, }
+    enum EVENT { NONE = 42, DISARM_SPOILER, ARM_SPOILER, MORE_SPOILER, LESS_SPOILER, }
+    enum GROUP { SPOILERS = 13, }
 
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
     struct AutopilotData
@@ -91,60 +91,60 @@ namespace FSInputMapper
         {
             // Autopilot
 
-            simConnect.AddToDataDefinition(DATA_DEFINITIONS.AUTOPILOT_DATA, "AUTOPILOT AIRSPEED HOLD VAR", "knots",
+            simConnect.AddToDataDefinition(DATA.AUTOPILOT_DATA, "AUTOPILOT AIRSPEED HOLD VAR", "knots",
                 SIMCONNECT_DATATYPE.FLOAT64, 2.5f, SimConnect.SIMCONNECT_UNUSED);
-            simConnect.AddToDataDefinition(DATA_DEFINITIONS.AUTOPILOT_DATA, "AUTOPILOT SPEED SLOT INDEX", "number",
+            simConnect.AddToDataDefinition(DATA.AUTOPILOT_DATA, "AUTOPILOT SPEED SLOT INDEX", "number",
                 SIMCONNECT_DATATYPE.FLOAT64, 0f, SimConnect.SIMCONNECT_UNUSED);
 
-            simConnect.AddToDataDefinition(DATA_DEFINITIONS.AUTOPILOT_DATA, "AUTOPILOT HEADING LOCK DIR", "degrees",
+            simConnect.AddToDataDefinition(DATA.AUTOPILOT_DATA, "AUTOPILOT HEADING LOCK DIR", "degrees",
                 SIMCONNECT_DATATYPE.FLOAT64, 2.5f, SimConnect.SIMCONNECT_UNUSED);
-            simConnect.AddToDataDefinition(DATA_DEFINITIONS.AUTOPILOT_DATA, "AUTOPILOT HEADING SLOT INDEX", "number",
+            simConnect.AddToDataDefinition(DATA.AUTOPILOT_DATA, "AUTOPILOT HEADING SLOT INDEX", "number",
                 SIMCONNECT_DATATYPE.FLOAT64, 0f, SimConnect.SIMCONNECT_UNUSED);
 
-            simConnect.AddToDataDefinition(DATA_DEFINITIONS.AUTOPILOT_DATA, "AUTOPILOT ALTITUDE LOCK VAR", "feet",
+            simConnect.AddToDataDefinition(DATA.AUTOPILOT_DATA, "AUTOPILOT ALTITUDE LOCK VAR", "feet",
                 SIMCONNECT_DATATYPE.FLOAT64, 100f, SimConnect.SIMCONNECT_UNUSED);
-            simConnect.AddToDataDefinition(DATA_DEFINITIONS.AUTOPILOT_DATA, "AUTOPILOT ALTITUDE SLOT INDEX", "number",
+            simConnect.AddToDataDefinition(DATA.AUTOPILOT_DATA, "AUTOPILOT ALTITUDE SLOT INDEX", "number",
                 SIMCONNECT_DATATYPE.FLOAT64, 0f, SimConnect.SIMCONNECT_UNUSED);
 
             //TODO: add vertical speed, "AUTOPILOT VERTICAL HOLD VAR" in "feet/minute", and maybe "AUTOPILOT VERTICAL HOLD"
 
-            simConnect.RegisterDataDefineStruct<AutopilotData>(DATA_DEFINITIONS.AUTOPILOT_DATA);
-            simConnect.RequestDataOnSimObject(REQUESTS.AUTOPILOT_DATA, DATA_DEFINITIONS.AUTOPILOT_DATA,
+            simConnect.RegisterDataDefineStruct<AutopilotData>(DATA.AUTOPILOT_DATA);
+            simConnect.RequestDataOnSimObject(REQUEST.AUTOPILOT_DATA, DATA.AUTOPILOT_DATA,
                 SimConnect.SIMCONNECT_OBJECT_ID_USER,
                 SIMCONNECT_PERIOD.SIM_FRAME, SIMCONNECT_DATA_REQUEST_FLAG.CHANGED, 0, 0, 0);
 
             // Spoilers
 
-            simConnect.AddToDataDefinition(DATA_DEFINITIONS.SPOILER_HANDLE, "SPOILERS HANDLE POSITION", "percent",
+            simConnect.AddToDataDefinition(DATA.SPOILER_HANDLE, "SPOILERS HANDLE POSITION", "percent",
                 SIMCONNECT_DATATYPE.FLOAT64, 0f, SimConnect.SIMCONNECT_UNUSED);
 
-            simConnect.AddToDataDefinition(DATA_DEFINITIONS.SPOILER_DATA, "SPOILERS HANDLE POSITION", "percent",
+            simConnect.AddToDataDefinition(DATA.SPOILER_DATA, "SPOILERS HANDLE POSITION", "percent",
                 SIMCONNECT_DATATYPE.FLOAT64, 0f, SimConnect.SIMCONNECT_UNUSED);
-            simConnect.AddToDataDefinition(DATA_DEFINITIONS.SPOILER_DATA, "SPOILERS ARMED", "Bool",
+            simConnect.AddToDataDefinition(DATA.SPOILER_DATA, "SPOILERS ARMED", "Bool",
                 SIMCONNECT_DATATYPE.FLOAT64, 0f, SimConnect.SIMCONNECT_UNUSED);
-            simConnect.RegisterDataDefineStruct<SpoilerData>(DATA_DEFINITIONS.SPOILER_DATA);
+            simConnect.RegisterDataDefineStruct<SpoilerData>(DATA.SPOILER_DATA);
 
-            simConnect.MapClientEventToSimEvent(EVENTS.MORE_SPOILER, "SPOILERS_TOGGLE");
-            simConnect.MapClientEventToSimEvent(EVENTS.LESS_SPOILER, "SPOILERS_ARM_TOGGLE");
+            simConnect.MapClientEventToSimEvent(EVENT.MORE_SPOILER, "SPOILERS_TOGGLE");
+            simConnect.MapClientEventToSimEvent(EVENT.LESS_SPOILER, "SPOILERS_ARM_TOGGLE");
 
-            simConnect.MapClientEventToSimEvent(EVENTS.ARM_SPOILER, "SPOILERS_ARM_ON");
-            simConnect.MapClientEventToSimEvent(EVENTS.DISARM_SPOILER, "SPOILERS_ARM_OFF");
+            simConnect.MapClientEventToSimEvent(EVENT.ARM_SPOILER, "SPOILERS_ARM_ON");
+            simConnect.MapClientEventToSimEvent(EVENT.DISARM_SPOILER, "SPOILERS_ARM_OFF");
 
-            simConnect.AddClientEventToNotificationGroup(GROUPS.SPOILERS, EVENTS.MORE_SPOILER, true);
-            simConnect.AddClientEventToNotificationGroup(GROUPS.SPOILERS, EVENTS.LESS_SPOILER, true);
-            simConnect.SetNotificationGroupPriority(GROUPS.SPOILERS, SimConnect.SIMCONNECT_GROUP_PRIORITY_HIGHEST_MASKABLE);
+            simConnect.AddClientEventToNotificationGroup(GROUP.SPOILERS, EVENT.MORE_SPOILER, true);
+            simConnect.AddClientEventToNotificationGroup(GROUP.SPOILERS, EVENT.LESS_SPOILER, true);
+            simConnect.SetNotificationGroupPriority(GROUP.SPOILERS, SimConnect.SIMCONNECT_GROUP_PRIORITY_HIGHEST_MASKABLE);
         }
 
         private void OnRecvEvent(SimConnect simConnect, SIMCONNECT_RECV_EVENT data)
         {
-            switch ((EVENTS)data.uEventID) {
-                case EVENTS.LESS_SPOILER:
-                    simConnect.RequestDataOnSimObject(REQUESTS.LESS_SPOILER, DATA_DEFINITIONS.SPOILER_DATA,
+            switch ((EVENT)data.uEventID) {
+                case EVENT.LESS_SPOILER:
+                    simConnect.RequestDataOnSimObject(REQUEST.LESS_SPOILER, DATA.SPOILER_DATA,
                         SimConnect.SIMCONNECT_OBJECT_ID_USER,
                         SIMCONNECT_PERIOD.ONCE, SIMCONNECT_DATA_REQUEST_FLAG.DEFAULT, 0, 0, 0);
                     break;
-                case EVENTS.MORE_SPOILER:
-                    simConnect.RequestDataOnSimObject(REQUESTS.MORE_SPOILER, DATA_DEFINITIONS.SPOILER_DATA,
+                case EVENT.MORE_SPOILER:
+                    simConnect.RequestDataOnSimObject(REQUEST.MORE_SPOILER, DATA.SPOILER_DATA,
                         SimConnect.SIMCONNECT_OBJECT_ID_USER,
                         SIMCONNECT_PERIOD.ONCE, SIMCONNECT_DATA_REQUEST_FLAG.DEFAULT, 0, 0, 0);
                     break;
@@ -153,36 +153,36 @@ namespace FSInputMapper
 
         private void OnRecvSimobjectData(SimConnect simConnect, SIMCONNECT_RECV_SIMOBJECT_DATA data)
         {
-            switch ((REQUESTS)data.dwRequestID)
+            switch ((REQUEST)data.dwRequestID)
             {
-                case REQUESTS.AUTOPILOT_DATA:
+                case REQUEST.AUTOPILOT_DATA:
                     AutopilotData autopilotData = (AutopilotData)data.dwData[0];
                     viewModel.AutopilotAltitude = (int)autopilotData.apAltitude;
                     viewModel.AltitudeManaged = autopilotData.apAltitudeSlot == 2;
                     break;
-                case REQUESTS.MORE_SPOILER:
+                case REQUEST.MORE_SPOILER:
                     SpoilerData spoilerData = (SpoilerData)data.dwData[0];
                     if (spoilerData.spoilersArmed != 0)
-                        SendEvent(GROUPS.SPOILERS, EVENTS.DISARM_SPOILER);
+                        SendEvent(GROUP.SPOILERS, EVENT.DISARM_SPOILER);
                     else
                         SetSpoilerHandlePosition(Math.Min(spoilerData.spoilersHandlePosition + 25.0, 100.0));
                     break;
-                case REQUESTS.LESS_SPOILER:
+                case REQUEST.LESS_SPOILER:
                     spoilerData = (SpoilerData)data.dwData[0];
                     if (spoilerData.spoilersHandlePosition > 0.0)
                         SetSpoilerHandlePosition(Math.Max(spoilerData.spoilersHandlePosition - 25.0, 0.0));
                     else if (spoilerData.spoilersArmed == 0)
-                        SendEvent(GROUPS.SPOILERS, EVENTS.ARM_SPOILER);
+                        SendEvent(GROUP.SPOILERS, EVENT.ARM_SPOILER);
                     break;
             }
         }
 
-        private void SendEvent(GROUPS group, EVENTS eventToSend) {
+        private void SendEvent(GROUP group, EVENT eventToSend) {
             simConnect?.TransmitClientEvent(SimConnect.SIMCONNECT_OBJECT_ID_USER, eventToSend, 0, group, 0);
         }
 
         private void SetSpoilerHandlePosition(double percent) {
-            simConnect?.SetDataOnSimObject(DATA_DEFINITIONS.SPOILER_HANDLE, SimConnect.SIMCONNECT_OBJECT_ID_USER, 0, percent);
+            simConnect?.SetDataOnSimObject(DATA.SPOILER_HANDLE, SimConnect.SIMCONNECT_OBJECT_ID_USER, 0, percent);
         }
 
         private IntPtr WndProc(IntPtr hWnd, int iMsg, IntPtr hWParam, IntPtr hLParam, ref bool bHandled)
