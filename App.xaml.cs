4d50bf6f8ae9bdadcbac41b66a8b26217446d8ce
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2020-09-17 18:59:24 +0100

    Handle unhandled exceptions.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,19 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Threading;
 
 //TODO: does https://stackoverflow.com/questions/54877352/dependency-injection-in-net-core-3-0-for-wpf make sense for us?
 
 namespace FSInputMapper
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this.Dispatcher.UnhandledException += (sender, args) => OnUnhandledException(args);
+        }
+
+        private void OnUnhandledException(DispatcherUnhandledExceptionEventArgs details)
+        {
+            System.Windows.MessageBox.Show(details.Exception.ToString(),
+                                  "Unhandled Exception",
+                                  MessageBoxButton.OK,
+                                  MessageBoxImage.Information);
+            details.Handled = true;
+        }
     }
 }
