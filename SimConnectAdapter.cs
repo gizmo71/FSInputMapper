56f727a911209f89447602f104137e3174690c8a
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2020-10-05 19:05:01 +0100

    Remove unused Using statements.

--- SimConnectAdapter.cs
+++ SimConnectAdapter.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows.Interop;
 using Microsoft.FlightSimulator.SimConnect;
