7e199ad02318f797f72d0ccc269ad9bceb5fc396
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2020-10-06 08:41:51 +0100

    Split SimConnectAdapter away from supporting code.

---
+++ SimConnectAttributes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.FlightSimulator.SimConnect;
+
+namespace FSInputMapper
+{
+
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public class DataAttribute : Attribute
+    {
+        public readonly Type DataType;
+        public DataAttribute(Type DataType) { this.DataType = DataType; }
+    }
+
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public class DataField : Attribute
+    {
+        public readonly string Variable;
+        public readonly string Units;
+        public readonly SIMCONNECT_DATATYPE Type;
+        public readonly float Epsilon;
+        public DataField(string variable, string units, SIMCONNECT_DATATYPE type, float epsilon)
+        {
+            Variable = variable;
+            Units = units;
+            Type = type; //TODO: do we need this, or can we infer it?
+            Epsilon = epsilon;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public class RequestAttribute : Attribute
+    {
+        public readonly DATA Data;
+        public readonly SIMCONNECT_PERIOD Period;
+        public readonly SIMCONNECT_DATA_REQUEST_FLAG Flag;
+        public RequestAttribute(DATA data, SIMCONNECT_PERIOD period)
+        {
+            Data = data;
+            Period = period;
+            Flag = Period == SIMCONNECT_PERIOD.ONCE
+                ? SIMCONNECT_DATA_REQUEST_FLAG.DEFAULT
+                : SIMCONNECT_DATA_REQUEST_FLAG.CHANGED;
+        }
+    }
+
+}
---
+++ SimConnectEnums.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.FlightSimulator.SimConnect;
+
+namespace FSInputMapper
+{
+
+    public enum DATA
+    {
+        // Stuff intended for struct-based multiple value requests:
+        [DataAttribute(typeof(ApData))] FCU_DATA = 69,
+        [DataAttribute(typeof(ApHdgSelData))] AP_HDG_SEL,
+        [DataAttribute(typeof(ApModeData))] AP_DATA,
+        [DataAttribute(typeof(SpoilerData))] SPOILER_DATA,
+        [DataAttribute(typeof(SpoilerHandle))] SPOILER_HANDLE,
+    }
+
+    enum REQUEST
+    {
+        [RequestAttribute(DATA.FCU_DATA, SIMCONNECT_PERIOD.SIM_FRAME)] FCU_DATA = 71,
+        [RequestAttribute(DATA.AP_HDG_SEL, SIMCONNECT_PERIOD.ONCE)] FCU_HDG_SEL,
+        [RequestAttribute(DATA.AP_DATA, SIMCONNECT_PERIOD.SIM_FRAME)] AP_DATA,
+        [RequestAttribute(DATA.SPOILER_DATA, SIMCONNECT_PERIOD.ONCE)] MORE_SPOILER,
+        [RequestAttribute(DATA.SPOILER_DATA, SIMCONNECT_PERIOD.ONCE)] LESS_SPOILER,
+    }
+
+    /*TODO: https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.categoryattribute?view=netcore-3.1
+      Way to identify specific things?
+      Would we be better to have a whole class for events and their recievers which includes an ID generator? */
+    enum EVENT
+    {
+        NONE = 42,
+        DISARM_SPOILER, ARM_SPOILER, MORE_SPOILER, LESS_SPOILER,
+        AP_SPEED_SLOT_SET, AP_SPD_UP, AP_SPD_DOWN,
+        AP_HEADING_SLOT_SET, AP_HDG_RIGHT, AP_HDG_LEFT, AP_HEADING_BUG_SET,
+        AP_ALTITUDE_SLOT_SET, AP_ALT_UP, AP_ALT_DOWN,
+        AP_TOGGLE_LOC, AP_TOGGLE_APPR, AP_AUTOTHRUST_ARM, AP_TOGGLE_FD,
+        FCU_VS_UP, FCU_VS_DOWN, FCU_VS_SET, FCU_VS_SLOT_SET, AP_PANEL_VS_ON
+    }
+    enum GROUP
+    {
+        SPOILERS = 13,
+        PRIORITY_STANDARD = 1900000000
+    }
+
+}
