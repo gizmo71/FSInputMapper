0fc7aee7baf803892168f751036573b893f9a547
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2020-10-31 16:13:41 +0000

    Add Baro listener.

---
+++ Data/Baro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.FlightSimulator.SimConnect;
+
+namespace FSInputMapper.Data
+{
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct BaroData
+    {
+        [SCStructField("BAROMETER PRESSURE", "Millibars", SIMCONNECT_DATATYPE.FLOAT32, 0.5f)]
+        public float barometerPressureMB;
+        [SCStructField("SEA LEVEL PRESSURE", "Millibars", SIMCONNECT_DATATYPE.FLOAT32, 0.5f)]
+        public float seaLevelPressureMB;
+        [SCStructField("KOHLSMAN SETTING MB", "Millibars", SIMCONNECT_DATATYPE.FLOAT32, 0.5f)]
+        public float kohlsmanMB; // This is the only settable one, via "KOHLSMAN_SET".
+        [SCStructField("KOHLSMAN SETTING HG", "inHg", SIMCONNECT_DATATYPE.FLOAT32, 0.5f)]
+        public float kohlsmanHg;
+    };
+
+    [Singleton]
+    public class BaroListener : DataListener<BaroData>, IRequestDataOnOpen
+    {
+
+        private readonly DebugConsole dc;
+
+        public BaroListener(DebugConsole dc)
+        {
+            this.dc = dc;
+        }
+
+        public SIMCONNECT_PERIOD GetInitialRequestPeriod()
+        {
+            return SIMCONNECT_PERIOD.SECOND;
+        }
+
+        public override void Process(SimConnect _, BaroData data)
+        {
+            dc.Text = $"baro {data.barometerPressureMB}MB MSL {data.seaLevelPressureMB}MB"
+                + $"\nKohlsman {data.kohlsmanMB}MB/{data.kohlsmanHg}Hg";
+        }
+
+    }
+
+}
