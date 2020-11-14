a2169f4a3e833f2e1cea9f78d8cc25f38bcc2fd5
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2020-11-14 20:50:32 +0000

    Be more accurate with baro settings, but don't reset so often.

--- Data/Baro.cs
+++ Data/Baro.cs
@@ -12,10 +12,10 @@ namespace FSInputMapper.Data
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
     public struct BaroData
     {
-        [SCStructField("SEA LEVEL PRESSURE", "Millibars", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public UInt32 seaLevelPressureMB;
-        [SCStructField("KOHLSMAN SETTING MB", "Millibars", SIMCONNECT_DATATYPE.INT32, 1f)]
-        public Int32 kohlsmanMB;
+        [SCStructField("SEA LEVEL PRESSURE", "Millibars", SIMCONNECT_DATATYPE.FLOAT32, 0.1f)]
+        public float seaLevelPressureMB;
+        [SCStructField("KOHLSMAN SETTING MB", "Millibars", SIMCONNECT_DATATYPE.FLOAT32, 0.1f)]
+        public float kohlsmanMB;
         [SCStructField("KOHLSMAN SETTING HG", "inHg", SIMCONNECT_DATATYPE.FLOAT32, 0.1f)]
         public float kohlsmanHg;
     };
@@ -42,10 +42,10 @@ namespace FSInputMapper.Data
         {
             dc.Text = $"MSL {data.seaLevelPressureMB}MB"
                 + $"\nKohlsman {data.kohlsmanMB}MB {data.kohlsmanHg.ToString("N2")}Hg";
-            if (data.seaLevelPressureMB != data.kohlsmanMB)
+            if (Math.Abs(data.seaLevelPressureMB - data.kohlsmanMB) > 0.666)
             {
                 //TODO: allow the user to turn off this automatic sync.
-                simConnect.SendEvent(kohlsmanSet, data.seaLevelPressureMB * 16);
+                simConnect.SendEvent(kohlsmanSet, (uint)(data.seaLevelPressureMB * 16.0));
                 dc.Text += $"\nAutomatically adjusted {DateTime.Now}";
             }
         }
