f2ff88de655a4ddd19904436ab23b33a4735bdc3
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2020-12-20 18:08:24 +0000

    Remove privacy page and add really simple model thingy.

--- Controlzmo/Pages/Index.cshtml.cs
+++ Controlzmo/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,9 +14,19 @@ namespace Controlzmo.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        [BindProperty]
+        [Display(Name = "First Field")]
+        //[DataType(DataType.Date)]
+        public String Field1 { get; set; }
+        [BindProperty]
+        [Display(Name = "Second Field")]
+        public String Field2 { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            Field1 = "value 1";
+            Field2 = "value 2";
         }
 
         public void OnGet()
