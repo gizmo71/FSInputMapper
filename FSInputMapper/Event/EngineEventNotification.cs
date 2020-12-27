f0137ce7af2ff950d313222f4cc08cd80f376808
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2020-12-27 14:13:01 +0000

    Start working on thottle mapping.

--- FSInputMapper/Event/EngineEventNotification.cs
+++ FSInputMapper/Event/EngineEventNotification.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.Security.Cryptography.X509Certificates;
-using System.Windows;
-using FSInputMapper.Data;
-using Microsoft.FlightSimulator.SimConnect;
+﻿using Microsoft.FlightSimulator.SimConnect;
 
 namespace FSInputMapper.Event
 {
 
 
     [Singleton]
-    public class Throttle1SetEvent : IEvent { public string SimEvent() { return "THROTTLE1_AXIS_SET_EX1"; } }
+    public class Throttle1SetEvent : IEvent { public string SimEvent() { return "THROTTLE1_SET"; } }
 
     [Singleton]
-    public class Throttle2SetEvent : IEvent { public string SimEvent() { return "THROTTLE2_AXIS_SET_EX1"; } }
+    public class Throttle2SetEvent : IEvent { public string SimEvent() { return "THROTTLE2_SET"; } }
 
     public abstract class ThrottleSetEventNotification : IEventNotification
     {
+        protected const uint MAGNITUDE_RANGE = 0x4000u;
         private readonly DebugConsole dc;
         private readonly IEvent trigger;
 
@@ -29,23 +26,36 @@ namespace FSInputMapper.Event
 
         public void OnRecieve(SimConnect simConnect, SIMCONNECT_RECV_EVENT data)
         {
-            simConnect.SendEvent((EVENT)data.uEventID, data.dwData);
-            double axis = ((int)data.dwData + 16384) / 327.68;
-            dc.Text = "Event ${(EVENT)data.uEventID} raw ${data.dwData} axis ${axis}";
+            double axis = (double)(data.dwData + MAGNITUDE_RANGE) - MAGNITUDE_RANGE;
+            dc.Text = $"Event {(EVENT)data.uEventID} raw {data.dwData} axis {axis}";
+            axis = map(axis) + MAGNITUDE_RANGE;
+            simConnect.SendEvent((EVENT)data.uEventID, (uint)axis - MAGNITUDE_RANGE);
         }
 
+        protected abstract double map(double axis);
     }
 
     [Singleton]
     public class Throttle1SetEventNotification : ThrottleSetEventNotification
     {
         public Throttle1SetEventNotification(DebugConsole dc, Throttle1SetEvent e) : base(dc, e) { }
+
+        protected override double map(double axis)
+        {
+            return axis;
+        }
     }
 
     [Singleton]
     public class Throttle2SetEventNotification : ThrottleSetEventNotification
     {
         public Throttle2SetEventNotification(DebugConsole dc, Throttle2SetEvent e) : base(dc, e) { }
+
+        protected override double map(double axis)
+        {
+            if (axis < 0) axis = -MAGNITUDE_RANGE;
+            return axis;
+        }
     }
 
 }
