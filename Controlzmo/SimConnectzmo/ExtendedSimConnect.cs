c1bb3e17bc87bdfeb9b62371aeeee429a8a45b08
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-01-06 08:46:29 +0000

    Add plumbing for data listening.

--- Controlzmo/SimConnectzmo/ExtendedSimConnect.cs
+++ Controlzmo/SimConnectzmo/ExtendedSimConnect.cs
@@ -26,6 +26,9 @@ namespace SimConnectzmo
             : base(szName, hWnd, UserEventWin32, waitHandle, 0) // 6 for over IP - can we make it timeout easier?
         {
             OnRecvOpen += Handle_OnRecvOpen;
+            OnRecvQuit += Handle_OnRecvQuit;
+            OnRecvSimobjectData += Handle_OnRecvSimobjectData;
+            OnRecvEvent += Handle_OnRecvEvent;
         }
 
         internal ExtendedSimConnect AssignIds(IServiceProvider serviceProvider)
@@ -53,6 +56,11 @@ namespace SimConnectzmo
                 RequestDataOnSimObject(request, request.GetInitialRequestPeriod());
         }
 
+        private void Handle_OnRecvQuit(SimConnect sender, SIMCONNECT_RECV data)
+        {
+            throw new NotImplementedException("Is it enough to tust through this? Is there anything else we should clean up?");
+        }
+
         private void RegisterDataStructs()
         {
             foreach (var type2Struct in typeToStruct!)
@@ -78,6 +86,30 @@ namespace SimConnectzmo
                             ? SIMCONNECT_DATA_REQUEST_FLAG.DEFAULT
                             : SIMCONNECT_DATA_REQUEST_FLAG.CHANGED;
             RequestDataOnSimObject(request, structId, SIMCONNECT_OBJECT_ID_USER, period, flag, 0, 0, 0);
+        }
+
+        private void Handle_OnRecvSimobjectData(SimConnect _, SIMCONNECT_RECV_SIMOBJECT_DATA data)
+        {
+            REQUEST request = (REQUEST)data.dwRequestID;
+            typeToRequest!
+                .Where(candidate => candidate.Value == request)
+                .Select(candidate => candidate.Key)
+                .Single()
+                .Process(this, data.dwData[0]);
+        }
+
+        private void Handle_OnRecvEvent(SimConnect sender, SIMCONNECT_RECV_EVENT data)
+        {
+#if false
+            EVENT e = (EVENT)data.uEventID;
+//debugConsole.Text = $"Received {e} = {Convert.ToString(data.dwData, 16)} {(int)data.dwData}s (of {data.dwSize})"
+//    + $"\n@{System.DateTime.Now}\nGroup ID {(GROUP)data.uGroupID} with ID {data.dwID} and version {data.dwVersion}";
+            foreach (KeyValuePair<IEventNotification, EVENT> entry in notificationsToEvent!
+                .Where<KeyValuePair<IEventNotification, EVENT>>(candidate => e == candidate.Value))
+            {
+                entry.Key.OnRecieve(simConnect, data);
+            }
+#endif
         }
     }
 }
