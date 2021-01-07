da89974fc4699fad3fc38217cd4778a50504839f
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-01-07 12:53:20 +0000

    Remove spoiler group from FSInputMapper too.

--- FSInputMapper/SimConnect/Enums.cs
+++ FSInputMapper/SimConnect/Enums.cs
@@ -4,8 +4,10 @@
     //TODO: convert to something else.
     public enum GROUP : uint
     {
+#if false
         [HighestMaskablePriorityGroup]
         SPOILERS = 13,
+#endif
         [HighestMaskablePriorityGroup]
         ENGINE,
         [HighestMaskablePriorityGroup]
