3a5a291aea5378bddfdadae8d5b783da62c372aa
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-01-14 08:29:23 +0000

    Dispose of any old ExtendedSimConnect.

--- Controlzmo/SimConnectzmo/Holder.cs
+++ Controlzmo/SimConnectzmo/Holder.cs
@@ -1,10 +1,31 @@
-﻿using Controlzmo;
+﻿using System;
+using Controlzmo;
 
 namespace SimConnectzmo
 {
     [Component]
-    public class SimConnectHolder
+    public class SimConnectHolder : IDisposable
     {
-        public ExtendedSimConnect? SimConnect { get; internal set; }
+        private ExtendedSimConnect? simConnect;
+        public ExtendedSimConnect? SimConnect
+        {
+            get { return simConnect; }
+            internal set
+            {
+                if (simConnect != value)
+                {
+                    if (simConnect != null)
+                    {
+                        simConnect.Dispose();
+                    }
+                    simConnect = value;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            SimConnect = null;
+        }
     }
 }
