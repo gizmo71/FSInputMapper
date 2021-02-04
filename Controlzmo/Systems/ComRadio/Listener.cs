f417fc80ba273ad73779609f2ea89131168ba640
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-02-04 22:22:03 +0000

    Split COM1 and COM2 and remove COM3.

--- Controlzmo/Systems/ComRadio/Listener.cs
+++ Controlzmo/Systems/ComRadio/Listener.cs
@@ -2,53 +2,73 @@
 using System.Runtime.InteropServices;
 using Controlzmo.Hubs;
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.Extensions.Logging;
 using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
 
 namespace Controlzmo.Systems.ComRadio
 {
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
-    public struct ComData
+    public struct Com1Data
     {
         [SimVar("COM ACTIVE FREQUENCY:1", "Frequency BCD32", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 com1ActiveFrequency;
+        public Int32 activeFrequency;
         [SimVar("COM STANDBY FREQUENCY:1", "Frequency BCD32", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 com1StandbyFrequency;
+        public Int32 standbyFrequency;
+    };
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct Com2Data
+    {
         [SimVar("COM ACTIVE FREQUENCY:2", "Frequency BCD32", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 com2ActiveFrequency;
+        public Int32 activeFrequency;
         [SimVar("COM STANDBY FREQUENCY:2", "Frequency BCD32", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 com2StandbyFrequency;
-        [SimVar("COM ACTIVE FREQUENCY:3", "Frequency BCD32", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 com3ActiveFrequency;
-        [SimVar("COM STANDBY FREQUENCY:3", "Frequency BCD32", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 com3StandbyFrequency;
+        public Int32 standbyFrequency;
     };
 
     [Component]
-    public class ComRadioListener : DataListener<ComData>, IRequestDataOnOpen
+    public class Com1RadioListener : DataListener<Com1Data>, IRequestDataOnOpen
     {
         private readonly IHubContext<ControlzmoHub, IControlzmoHub> hub;
 
-        public ComRadioListener(IHubContext<ControlzmoHub, IControlzmoHub> hub)
+        public Com1RadioListener(IHubContext<ControlzmoHub, IControlzmoHub> hub)
         {
             this.hub = hub;
         }
 
         public SIMCONNECT_PERIOD GetInitialRequestPeriod() => SIMCONNECT_PERIOD.VISUAL_FRAME;
 
-        public override void Process(ExtendedSimConnect simConnect, ComData data)
+        public override void Process(ExtendedSimConnect simConnect, Com1Data data)
         {
-            hub.Clients.All.SetFromSim("com1active", FormatFrequency(data.com1ActiveFrequency));
-            hub.Clients.All.SetFromSim("com1standby", FormatFrequency(data.com1StandbyFrequency));
+            hub.Clients.All.SetFrequencyFromSim("com1active", data.activeFrequency);
+            hub.Clients.All.SetFrequencyFromSim("com1standby", data.standbyFrequency);
+        }
+    }
 
-            hub.Clients.All.SetFromSim("com2active", FormatFrequency(data.com2ActiveFrequency));
-            hub.Clients.All.SetFromSim("com2standby", FormatFrequency(data.com2StandbyFrequency));
+    [Component]
+    public class Com2RadioListener : DataListener<Com2Data>, IRequestDataOnOpen
+    {
+        private readonly IHubContext<ControlzmoHub, IControlzmoHub> hub;
 
-            hub.Clients.All.SetFromSim("com3active", FormatFrequency(data.com3ActiveFrequency));
-            hub.Clients.All.SetFromSim("com3standby", FormatFrequency(data.com3StandbyFrequency));
+        public Com2RadioListener(IHubContext<ControlzmoHub, IControlzmoHub> hub)
+        {
+            this.hub = hub;
         }
 
-        private string FormatFrequency(int bcdHz) => String.Format("{0:X03}.{1:X03}", (bcdHz >> 16) & 0xFFF, (bcdHz >> 4) & 0xFFF);
+        public SIMCONNECT_PERIOD GetInitialRequestPeriod() => SIMCONNECT_PERIOD.VISUAL_FRAME;
+
+        public override void Process(ExtendedSimConnect simConnect, Com2Data data)
+        {
+            hub.Clients.All.SetFrequencyFromSim("com2active", data.activeFrequency);
+            hub.Clients.All.SetFrequencyFromSim("com2standby", data.standbyFrequency);
+        }
+    }
+
+    internal static class FrequencyExtensions
+    {
+        internal static void SetFrequencyFromSim(this IControlzmoHub hub, string field, int bcdHz)
+        {
+            string asString = String.Format("{0:X03}.{1:X03}", (bcdHz >> 16) & 0xFFF, (bcdHz >> 4) & 0xFFF);
+            hub.SetFromSim(field, asString);
+        }
     }
 }
