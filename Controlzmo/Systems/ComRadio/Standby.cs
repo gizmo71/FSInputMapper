271070ec0e3f35e887b1e36f73603be9969c2c9c
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-02-05 21:45:50 +0000

    Add experimental bump when setting some of the COM frequencies.

--- Controlzmo/Systems/ComRadio/Standby.cs
+++ Controlzmo/Systems/ComRadio/Standby.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using Controlzmo.Hubs;
 using SimConnectzmo;
 
@@ -7,8 +8,13 @@ namespace Controlzmo.Systems.ComRadio
     [Component]
     public class Com1StandbyRadioSetEvent : IEvent
     {
-        // In Hz, so 123.245 would be 123245000 - possibly in BCD (effectively hex)
         public string SimEvent() => "COM_STBY_RADIO_SET";
+    }
+
+    [Component]
+    public class Com1StandbyRadioBumpEvent : IEvent
+    {
+        public string SimEvent() => "COM_RADIO_FRACT_INC";
     }
 
     [Component]
@@ -18,52 +24,74 @@ namespace Controlzmo.Systems.ComRadio
     }
 
     [Component]
+    public class Com2StandbyRadioBumpEvent : IEvent
+    {
+        public string SimEvent() => "COM2_RADIO_FRACT_INC";
+    }
+
+    [Component]
     public class Com3StandbyRadioSetEvent : IEvent
     {
         public string SimEvent() => "COM3_STBY_RADIO_SET";
     }
 
+    [Component]
+    public class Com3StandbyRadioBumpEvent : IEvent
+    {
+        public string SimEvent() => "COM3_RADIO_FRACT_INC";
+    }
+
     public abstract class AbstractComStandby : ISettable<string?>
     {
+        // In Hz, so 123.245 would be 123245000 - possibly in BCD (effectively hex)
+        // Currently, MSFS doesn't support setting the first or last digit directly. :-(
         private readonly IEvent setEvent;
+        private readonly IEvent bumpEvent;
 
-        protected AbstractComStandby(IEvent setEvent) => this.setEvent = setEvent;
+        protected AbstractComStandby(IEvent setEvent, IEvent bumpEvent)
+        {
+            this.setEvent = setEvent;
+            this.bumpEvent = bumpEvent;
+        }
 
         public abstract string GetId();
+
+        // https://www.caa.co.uk/General-aviation/Aircraft-ownership-and-maintenance/8-33-kHz-radios/
+        // 00, 05, 10, 15, 25, 30, 35, 40, 50, 55, 60, 65, 75, 80. 85, 90
+        private static readonly Regex NeedsBump = new Regex(@"[013568]5$", RegexOptions.Compiled);
 
         public void SetInSim(ExtendedSimConnect simConnect, string? newFrequencyString)
         {
             var newFrequency = Decimal.Parse(newFrequencyString!);
             uint kHz = Decimal.ToUInt32(Decimal.Multiply(newFrequency, new Decimal(1000)));
             string bcdHex = kHz.ToString("D6");
-            // Currently, MSFS doesn't support setting the first or last digit directly. :-(
-            //TODO: if we can't get this to work with 8.33kHz spacing, try COM_RADIO_WHOLE_INC/COM_RADIO_WHOLE_DEC and COM_RADIO_FRACT_INC/COM_RADIO_FRACT_DEC
             bcdHex = bcdHex.Substring(1, 4);
-            //throw new Exception($"hex {bcdHex}");
             uint bcd = uint.Parse(bcdHex, System.Globalization.NumberStyles.HexNumber);
             simConnect.SendEvent(setEvent, bcd);
-            //return bcdHex;
+            if (NeedsBump.IsMatch(bcdHex)) {
+                simConnect.SendEvent(bumpEvent);
+            }
         }
     }
 
     [Component]
     public class Com1Standby : AbstractComStandby
     {
-        public Com1Standby(Com1StandbyRadioSetEvent setEvent) : base(setEvent) { }
+        public Com1Standby(Com1StandbyRadioSetEvent set, Com1StandbyRadioBumpEvent bump) : base(set, bump) { }
         public override string GetId() => "com1standby";
     }
 
     [Component]
     public class Com2Standby : AbstractComStandby
     {
-        public Com2Standby(Com2StandbyRadioSetEvent setEvent) : base(setEvent) { }
+        public Com2Standby(Com2StandbyRadioSetEvent set, Com2StandbyRadioBumpEvent bump) : base(set, bump) { }
         public override string GetId() => "com2standby";
     }
 
     [Component]
     public class Com3Standby : AbstractComStandby
     {
-        public Com3Standby(Com3StandbyRadioSetEvent setEvent) : base(setEvent) { }
+        public Com3Standby(Com3StandbyRadioSetEvent set, Com3StandbyRadioBumpEvent bump) : base(set, bump) { }
         public override string GetId() => "com3standby";
     }
 }
