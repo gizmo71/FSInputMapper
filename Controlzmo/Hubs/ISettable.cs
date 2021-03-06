5e1e4930cd4d6d973f8417193cd86ccae2adad48
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-03-06 14:29:12 +0000

    Allow null to be passed to clients.

--- Controlzmo/Hubs/ISettable.cs
+++ Controlzmo/Hubs/ISettable.cs
@@ -13,7 +13,7 @@ namespace Controlzmo.Hubs
     public interface ISettable<ValueType> : ISettable
     {
         Type ISettable.GetValueType() => typeof(ValueType);
-        void ISettable.SetInSim(ExtendedSimConnect simConnect, object? value) => SetInSim(simConnect, (ValueType)value);
+        void ISettable.SetInSim(ExtendedSimConnect simConnect, object? value) => SetInSim(simConnect, (ValueType?)value);
         void SetInSim(ExtendedSimConnect simConnect, ValueType? value);
 
     }
