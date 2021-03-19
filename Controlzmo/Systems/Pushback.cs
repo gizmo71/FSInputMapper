f8510fab5edbbaf00ec2d445bb5b5ff187a15018
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-03-19 16:03:03 +0000

    Move some more pushback stuff around.

--- Controlzmo/Systems/Pushback.cs
+++ Controlzmo/Systems/Pushback.cs
@@ -41,9 +41,9 @@ namespace Controlzmo.Systems.Pushback
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
     public struct PushbackWaitData
     {
-        [SimVar("PUSHBACK WAIT", "Bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
+        [SimVar("Pushback Wait", "Bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
         // In theory this should say true if waiting for the tug after calling it, but doesn't seem to work.
-        // The EFB code suggests that after calling the tug, this variable is no longer useful, which matches observations.
+        // The EFB code suggests that after calling the tug, this variable is no longer useful, which matches observations, except that it seems useless before too!
         public int pushbackWait;
     }
 
@@ -67,11 +67,6 @@ namespace Controlzmo.Systems.Pushback
         // In theory this is settable. Starts out as 3.
         public int pushbackState;
 
-        [SimVar("PUSHBACK WAIT", "Bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        // In theory this should say true if waiting for the tug after calling it, but doesn't seem to work.
-        // The EFB code suggests that after calling the tug, this variable is no longer useful, which matches observations.
-        public int pushbackWait;
-
         [SimVar("PUSHBACK ANGLE", "Degrees", SIMCONNECT_DATATYPE.INT32, 0.01f)]
         // Not settable; default values seem to be 20 (tail to the right, heading decreasing) or -20 (tail to the left, heading increasing)
         public int pushbackAngle;
@@ -79,21 +74,12 @@ namespace Controlzmo.Systems.Pushback
         [SimVar("PLANE HEADING DEGREES TRUE", "Degrees", SIMCONNECT_DATATYPE.INT32, 0.01f)]
         public int trueHeading;
 
-        [SimVar("GROUND VELOCITY", "Knots", SIMCONNECT_DATATYPE.FLOAT32, 0.01f)]
-        public float groundKnots;
-
         [SimVar("TOTAL WORLD VELOCITY", "Feet per second", SIMCONNECT_DATATYPE.FLOAT32, 0.2f)]
         public float velocityTotal;
 
         [SimVar("PUSHBACK ATTACHED", "Bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
         // State 0 and this as 1 immediately after toggling pushback, even before tug is attached.
         public int pushbackAttached;
-
-        [SimVar("Brake Parking Position", "Bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public int parkingBrakePosition;
-        /*simClient.AddToDataDefinition(DefinitionsEnum.RotationX, "Rotation Velocity Body X", "feet per second", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
-          simClient.AddToDataDefinition(DefinitionsEnum.RotationY, "Rotation Velocity Body Y", "feet per second", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
-          simClient.AddToDataDefinition(DefinitionsEnum.RotationZ, "Rotation Velocity Body Z", "feet per second", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);*/
     };
 
     //[Component]
@@ -110,8 +96,8 @@ namespace Controlzmo.Systems.Pushback
 
         public override void Process(ExtendedSimConnect simConnect, PushbackData data)
         {
-            System.Console.Error.WriteLine($"Pushback: state={data.pushbackState} attached={data.pushbackAttached} wait={data.pushbackWait}, angle={data.pushbackAngle},\n"
-                + $"\theading={data.trueHeading}, knots={data.groundKnots}, parkBrake={data.parkingBrakePosition}, velocity={data.velocityTotal}");
+            System.Console.Error.WriteLine($"Pushback: state={data.pushbackState}, attached={data.pushbackAttached}, "
+                + $"angle={data.pushbackAngle}, heading={data.trueHeading}, velocity={data.velocityTotal}");
             hub.Clients.All.SetFromSim("pushbackState", data.pushbackState);
             hub.Clients.All.SetFromSim("pushbackAngle", data.pushbackAngle);
             hub.Clients.All.SetFromSim("trueHeading", data.trueHeading);
