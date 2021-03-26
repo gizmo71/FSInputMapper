fbe005fb255129b73f4cd06e3937edfce7c8248f
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-03-26 16:56:01 +0000

    Use _HZ version of COM radio setter.

--- Controlzmo/Systems/ComRadio/Standby.cs
+++ Controlzmo/Systems/ComRadio/Standby.cs
@@ -8,92 +8,45 @@ namespace Controlzmo.Systems.ComRadio
     [Component]
     public class Com1StandbyRadioSetEvent : IEvent
     {
-        public string SimEvent() => "COM_STBY_RADIO_SET";
-        // or with more precision?   COM_STBY_RADIO_SET_HZ
-    }
-
-    [Component]
-    public class Com1StandbyRadioBumpEvent : IEvent
-    {
-        public string SimEvent() => "COM_RADIO_FRACT_INC";
+        public string SimEvent() => "COM_STBY_RADIO_SET_HZ";
     }
 
     [Component]
     public class Com2StandbyRadioSetEvent : IEvent
     {
-        public string SimEvent() => "COM2_STBY_RADIO_SET";
-        // or with more precision?   COM2_STBY_RADIO_SET_HZ
-        // If we could set LVars, L:A32NX_RMP_L_VHF2_STANDBY_FREQUENCY
-    }
-
-    [Component]
-    public class Com2StandbyRadioBumpEvent : IEvent
-    {
-        public string SimEvent() => "COM2_RADIO_FRACT_INC";
-    }
-
-    [Component]
-    public class Com3StandbyRadioSetEvent : IEvent
-    {
-        public string SimEvent() => "COM3_STBY_RADIO_SET";
-    }
-
-    [Component]
-    public class Com3StandbyRadioBumpEvent : IEvent
-    {
-        public string SimEvent() => "COM3_RADIO_FRACT_INC";
+        public string SimEvent() => "COM2_STBY_RADIO_SET_HZ";
     }
 
     public abstract class AbstractComStandby : ISettable<string?>
     {
-        // In Hz, so 123.245 would be 123245000 - possibly in BCD (effectively hex)
-        // Currently, MSFS doesn't support setting the first or last digit directly. :-(
         private readonly IEvent setEvent;
-        private readonly IEvent bumpEvent;
 
-        protected AbstractComStandby(IEvent setEvent, IEvent bumpEvent)
+        protected AbstractComStandby(IEvent setEvent)
         {
             this.setEvent = setEvent;
-            this.bumpEvent = bumpEvent;
         }
 
         public abstract string GetId();
 
-        // https://www.caa.co.uk/General-aviation/Aircraft-ownership-and-maintenance/8-33-kHz-radios/
-        private static readonly Regex NeedsBump = new Regex(@"[013568]5$", RegexOptions.Compiled);
-
         public void SetInSim(ExtendedSimConnect simConnect, string? newFrequencyString)
         {
-// LVAR A32NX_RMP_L_VHF2_STANDBY might allow setting directly
             var newFrequency = Decimal.Parse(newFrequencyString!);
-            uint kHz = Decimal.ToUInt32(Decimal.Multiply(newFrequency, new Decimal(1000)));
-            string bcdHex = kHz.ToString("D6");
-            uint bcdCentre = uint.Parse(bcdHex.Substring(1, 4), System.Globalization.NumberStyles.HexNumber);
-            simConnect.SendEvent(setEvent, bcdCentre);
-            if (NeedsBump.IsMatch(bcdHex)) {
-                simConnect.SendEvent(bumpEvent);
-            }
+            uint hz = Decimal.ToUInt32(Decimal.Multiply(newFrequency, new Decimal(1000000)));
+            simConnect.SendEvent(setEvent, hz);
         }
     }
 
     [Component]
     public class Com1Standby : AbstractComStandby
     {
-        public Com1Standby(Com1StandbyRadioSetEvent set, Com1StandbyRadioBumpEvent bump) : base(set, bump) { }
+        public Com1Standby(Com1StandbyRadioSetEvent set) : base(set) { }
         public override string GetId() => "com1standby";
     }
 
     [Component]
     public class Com2Standby : AbstractComStandby
     {
-        public Com2Standby(Com2StandbyRadioSetEvent set, Com2StandbyRadioBumpEvent bump) : base(set, bump) { }
+        public Com2Standby(Com2StandbyRadioSetEvent set) : base(set) { }
         public override string GetId() => "com2standby";
-    }
-
-    [Component]
-    public class Com3Standby : AbstractComStandby
-    {
-        public Com3Standby(Com3StandbyRadioSetEvent set, Com3StandbyRadioBumpEvent bump) : base(set, bump) { }
-        public override string GetId() => "com3standby";
     }
 }
