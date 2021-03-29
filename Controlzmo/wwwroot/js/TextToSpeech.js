78406031fe0307d1aafc351b4e9216640f853d76
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-03-29 20:52:05 +0100

    Temporarily reenable text to speed to test on phone.

--- Controlzmo/wwwroot/js/TextToSpeech.js
+++ Controlzmo/wwwroot/js/TextToSpeech.js
@@ -1,6 +1,19 @@
 ﻿// https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
 var synth = window.speechSynthesis;
-var utterance = new SpeechSynthesisUtterance('V1');
-utterance.pitch = 1; // 0 to 2, with 1 as default
-utterance.rate = 1.5; // 0.1 to 10, with 1 as default
+var utterance = new SpeechSynthesisUtterance('eighty knots V1');
+//utterance.pitch = 1; // 0 to 2, with 1 as default
+utterance.rate = 1.25; // 0.1 to 10, with 1 as default
 synth.speak(utterance);
+
+utterance = new SpeechSynthesisUtterance('Rotate');
+utterance.rate = 1.25; // 0.1 to 10, with 1 as default
+synth.speak(utterance);
+
+/*
+If this works on the phone, consider getting L:AIRLINER_V1_SPEED and L:AIRLINER_VR_SPEED and calling them.
+Maybe 80 knots too, all using "AIRSPEED INDICATED" and something to detect takeoff mode.
+Would have to have a WASM module to send client events we could RX.
+On landing, would be good to detect spoilers popping, reversers, and decel light.
+https://github.com/flybywiresim/a32nx/blob/autopilot/docs/a320-simvars.md
+https://github.com/flybywiresim/a32nx/blob/autopilot/docs/a320-events.md
+*/
