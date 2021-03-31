3195f29eb6a31a83960213b8a06e682c7c4048ac
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-03-31 14:49:03 +0100

    Move some notes out to the README.

---
+++ Controlzmo/README.md
@@ -0,0 +1,28 @@
+Gizmo's Controlzmo
+==================
+
+Designed mostly to enhance interaction with the default A320neo in MSFS (aka FSFS2020).
+
+When running, it will attempt to attached SimConnect to MSFS every couple of seconds until it succeeds, and should cope gracefully if it exits.
+
+Radio
+-----
+
+TODO
+
+Lights
+------
+
+TODO
+
+TODO
+----
+
+Would have to have a WASM module to send client events we could RX.
+
+On landing, would be good to detect spoilers popping, reversers, and decel light.
+
+https://github.com/flybywiresim/a32nx/blob/autopilot/docs/a320-simvars.md
+https://github.com/flybywiresim/a32nx/blob/autopilot/docs/a320-events.md
+
+APU controls - not part of PM - A32NX_APU_BLEED_AIR_VALVE_OPEN
--- Controlzmo/Systems/PilotMonitoring.cs
+++ Controlzmo/Systems/PilotMonitoring.cs
@@ -6,14 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
 
-/*
-If this works on the phone, consider getting L:AIRLINER_V1_SPEED and L:AIRLINER_VR_SPEED and calling them.
-Maybe 80 knots too, all using "AIRSPEED INDICATED" and something to detect takeoff mode.
-Would have to have a WASM module to send client events we could RX.
-On landing, would be good to detect spoilers popping, reversers, and decel light.
-https://github.com/flybywiresim/a32nx/blob/autopilot/docs/a320-simvars.md
-https://github.com/flybywiresim/a32nx/blob/autopilot/docs/a320-events.md
-*/
 namespace Controlzmo.Systems.PilotMonitoring
 {
     internal enum CLIENT_ENUM { PLACEHOLDER = 123 }
