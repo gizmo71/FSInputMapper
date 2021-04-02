67decf5f7b48abab3c9194aeba59ea6d991f6682
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-04-02 09:20:49 +0100

    Comment about ON ANY RUNWAY

--- Controlzmo/Systems/PilotMonitoring/OnGround.cs
+++ Controlzmo/Systems/PilotMonitoring/OnGround.cs
@@ -1,9 +1,10 @@
 ﻿namespace Controlzmo.Systems.PilotMonitoring
 {
-    //TODO: just monitor on ground status and start/stop other PM listeners based on that
+//TODO: just monitor on ground status and start/stop other PM listeners based on that
 /*On landing, would be good to detect reversers (how?),
-spoilers popping (see A320_Neo_LowerECAM_FTCL.js, just using SPOILERS LEFT/RIGHT POSITION and ON GROUND?)*/
-    public class OnGround
+spoilers popping (see A320_Neo_LowerECAM_FTCL.js, just using SPOILERS LEFT/RIGHT POSITION and SIM ON GROUND?)
+How about "ON ANY RUNWAY"? */
+    public class OnGroundListener
     {
     }
 }
