917407d925bf6af9ab20d924ab3db8756c1641cc
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-04-03 16:24:59 +0100

    Avoid takeoff calls on landing.

--- Controlzmo/Systems/PilotMonitoring/TakeoffSpeeds.cs
+++ Controlzmo/Systems/PilotMonitoring/TakeoffSpeeds.cs
@@ -21,10 +21,10 @@ namespace Controlzmo.Systems.PilotMonitoring
         private readonly IHubContext<ControlzmoHub, IControlzmoHub> hubContext;
         private readonly LocalVarsListener localVarsListener;
 
-        bool wasAbove35 = false;
-        bool wasAbove80 = false;
-        bool wasAboveV1 = false;
-        bool wasAboveVR = false;
+        bool? wasAbove35 = null;
+        bool? wasAbove80 = null;
+        bool? wasAboveV1 = null;
+        bool? wasAboveVR = null;
 
         public TakeOffListener(IServiceProvider serviceProvider)
         {
@@ -36,27 +36,28 @@ namespace Controlzmo.Systems.PilotMonitoring
         private void OnGroundHandler(ExtendedSimConnect simConnect, bool isOnGround)
         {
             simConnect.RequestDataOnSimObject(this, isOnGround ? SIMCONNECT_PERIOD.SECOND : SIMCONNECT_PERIOD.NEVER);
-//TODO: don't reset these if we were previously in the air, otherwise the 35/80 calls will sound again.
-            wasAbove35 = wasAbove80 = wasAboveV1 = wasAboveVR = false;
+            wasAbove35 = wasAbove80 = wasAboveV1 = wasAboveVR = null;
         }
 
         public override void Process(ExtendedSimConnect simConnect, TakeOffData data)
         {
-            System.Console.Error.WriteLine($"Takeoff: KIAS {data.kias}, V1/VR {localVarsListener.localVars.v1}/{localVarsListener.localVars.vr}");
+System.Console.Error.WriteLine($"Takeoff: KIAS {data.kias}, V1/VR {localVarsListener.localVars.v1}/{localVarsListener.localVars.vr}");
+            if (data.kias < 30)
+                wasAbove35 = wasAbove80 = wasAboveV1 = wasAboveVR = false;
             setAndCallIfRequired(35, data.kias, "airspeed alive", ref wasAbove35);
             setAndCallIfRequired(80, data.kias, "eighty knots", ref wasAbove80);
             setAndCallIfRequired(localVarsListener.localVars.v1, data.kias, "vee one", ref wasAboveV1);
             setAndCallIfRequired(localVarsListener.localVars.vr, data.kias, "rotate", ref wasAboveVR);
         }
 
-        private void setAndCallIfRequired(short calledSpeed, float actualSpeed, string call, ref bool wasAbove)
+        private void setAndCallIfRequired(short calledSpeed, float actualSpeed, string call, ref bool? wasAbove)
         {
             bool isAbove = calledSpeed > 0 && actualSpeed >= calledSpeed;
-            if (isAbove && !wasAbove)
+            if (isAbove && wasAbove == false)
             {
                 hubContext.Clients.All.Speak(call);
+                wasAbove = true;
             }
-            wasAbove = isAbove;
         }
     }
 }
