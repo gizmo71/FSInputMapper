dac5a576c8d24b7926773138dcbaf99f0fe8bc01
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-04-05 13:16:16 +0100

    Split SimVar into ClientVar base.

--- Controlzmo/SimConnectzmo/SimVar.cs
+++ Controlzmo/SimConnectzmo/SimVar.cs
@@ -4,19 +4,28 @@ using Microsoft.FlightSimulator.SimConnect;
 namespace SimConnectzmo
 {
     [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
-    public class SimVarAttribute : Attribute
+    public class ClientVarAttribute : Attribute
+    {
+        public readonly float Epsilon;
+
+        public ClientVarAttribute(float epsilon)
+        {
+            Epsilon = epsilon;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public class SimVarAttribute : ClientVarAttribute
     {
         public readonly string Variable;
         public readonly string? Units;
         public readonly SIMCONNECT_DATATYPE Type;
-        public readonly float Epsilon;
 
-        public SimVarAttribute(string variable, string? units, SIMCONNECT_DATATYPE type, float epsilon)
+        public SimVarAttribute(string variable, string? units, SIMCONNECT_DATATYPE type, float epsilon) : base(epsilon)
         {
             Variable = variable;
             Units = units;
             Type = type; //TODO: do we need this, or can we infer it?
-            Epsilon = epsilon;
         }
     }
 }
