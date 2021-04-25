4a8a964868d78fb2812874253d94c4eec06c394d
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-04-25 11:19:27 +0100

    Autopilot not working. :-(

--- Controlzmo/Systems/Autopilot.cs
+++ Controlzmo/Systems/Autopilot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Controlzmo.SimConnectzmo;
+using Controlzmo.Systems.JetBridge;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
@@ -7,21 +8,23 @@ using SimConnectzmo;
 namespace Controlzmo.Systems.Autopilot
 {
     [Component]
-    public class Autopilot1Active : LVar
+    public class Autopilot1Active : LVar, IOnSimConnection
     {
         public Autopilot1Active(IServiceProvider serviceProvider) : base(serviceProvider) { }
         protected override string LVarName() => "A32NX_AUTOPILOT_1_ACTIVE";
         protected override int Milliseconds() => 167;
         protected override double Default() => -1.0;
+        public void OnConnection(ExtendedSimConnect simConnect) => Request(simConnect);
     }
 
     [Component]
-    public class Autopilot2Active : LVar
+    public class Autopilot2Active : LVar, IOnSimConnection
     {
         public Autopilot2Active(IServiceProvider serviceProvider) : base(serviceProvider) { }
         protected override string LVarName() => "A32NX_AUTOPILOT_2_ACTIVE";
         protected override int Milliseconds() => 167;
         protected override double Default() => -1.0;
+        public void OnConnection(ExtendedSimConnect simConnect) => Request(simConnect);
     }
 
     [Component]
@@ -43,6 +46,7 @@ namespace Controlzmo.Systems.Autopilot
         private readonly Autopilot2Active ap2Active;
         private readonly Autopilot1OnPush ap1Push;
         private readonly Autopilot2OnPush ap2Push;
+        private readonly JetBridgeSender jetbridge;
 
         public AutopilotOnEvent(IServiceProvider sp)
         {
@@ -50,6 +54,7 @@ namespace Controlzmo.Systems.Autopilot
             ap2Active = sp.GetRequiredService<Autopilot2Active>();
             ap1Push = sp.GetRequiredService<Autopilot1OnPush>();
             ap2Push = sp.GetRequiredService<Autopilot2OnPush>();
+            jetbridge = sp.GetRequiredService<JetBridgeSender>();
         }
 
         public string SimEvent() => "AUTOPILOT_ON";
@@ -57,10 +62,14 @@ namespace Controlzmo.Systems.Autopilot
 
         public void OnRecieve(ExtendedSimConnect simConnect, SIMCONNECT_RECV_EVENT data)
         {
-            if (ap1Active == 0.0)
+            double? ap1Value = ap1Active;
+System.Console.Error.WriteLine($"AP On; AP1 {ap1Value} want? {ap1Active == 0.0}, AP2 {(double?)ap2Active} want? {ap2Active == 0.0}");
+            if (ap1Value == 0.0)
                 simConnect.SendEvent(ap1Push);
+                //jetbridge.Execute(simConnect, "(>K:A32NX.FCU_AP_1_PUSH)");
             else if (ap2Active == 0.0)
                 simConnect.SendEvent(ap2Push);
+                //jetbridge.Execute(simConnect, "(>K:A32NX.FCU_AP_2_PUSH)");
             // For non-A32NX, resend normal event.
             simConnect.SendEvent(this);
         }
