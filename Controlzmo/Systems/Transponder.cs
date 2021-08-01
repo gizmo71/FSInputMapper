95c6c96bf7ed816a81a8c60f759683f71da9b848
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-08-01 14:53:30 +0100

    More transponder comments.

--- Controlzmo/Systems/Transponder.cs
+++ Controlzmo/Systems/Transponder.cs
@@ -14,7 +14,7 @@ using SimConnectzmo;
   Setting to 1 from On always turns it to Standby, regardless of Alt Rptg.
   If Alt Rptg is On, only 4 will set it to On.
   If Alt Rptg is Off, only 3 will set it to On.
-* Alt Rptg L:A32NX_SWITCH_ATC_ALT - can be set to 0 off or 1 on.
+* Alt Rptg L:A32NX_SWITCH_ATC_ALT - can be set to 0 off, 1 auto, or 2 on.
 * Ident? (local event `A320_Neo_ATC_BTN_IDENT`?) */
 namespace Controlzmo.Systems.Transponder
 {
