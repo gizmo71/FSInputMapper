772b77aa4634dbf25da8fb7475ac331ced31a925
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-08-11 19:54:26 +0100

    Add Serial to KeepAliveWorker.

--- Controlzmo/Serial/Serial.cs
+++ Controlzmo/Serial/Serial.cs
@@ -1,68 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Controlzmo.Hubs;
-using SimConnectzmo;
+using System.ComponentModel;
 using System.IO.Ports;
-using System.Threading;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Controlzmo.Serial
 {
     [Component]
-    public class Serial : CreateOnStartup, IDisposable
+    public class Serial : KeepAliveWorker, CreateOnStartup
     {
+        private readonly ILogger _logger;
         private readonly SerialPort _serialPort;
-        private readonly Thread readThread; // Convert to BackgroundWorker a la Adapter
-        private bool running = true;
 
-        public Serial()
+        public Serial(IServiceProvider serviceProvider) : base(serviceProvider)
         {
+            _logger = serviceProvider.GetRequiredService<ILogger<Serial>>();
+
             _serialPort = new SerialPort(portName: "COM3", baudRate: 115200, parity: Parity.None, dataBits: 8);
             _serialPort.StopBits = StopBits.One;
             _serialPort.Handshake = Handshake.RequestToSend;
             _serialPort.DtrEnable = true;
             _serialPort.ReadTimeout = 500;
             _serialPort.WriteTimeout = 500;
-            readThread = new Thread(Read);
-            readThread.IsBackground = true;
-            readThread.Start();
         }
 
-        public void Read()
+        protected override void OnStart(object? sender, DoWorkEventArgs args)
         {
-            //TODO: like SimConnect, we want to open when we can, sleep when we can't, and transition between both.
+            _serialPort.Open();
+        }
+
+        private byte[] writeData = { 0 };
+
+        protected override void OnLoop(object? sender, DoWorkEventArgs args)
+        {
             try
             {
-                _serialPort.Open();
+                string message = _serialPort.ReadLine();
+                Console.Error.WriteLine(message);
             }
-            catch (Exception e)
+            catch (TimeoutException)
             {
-                Console.Error.WriteLine($"Failed to open {_serialPort} with {e}");
-                return;
-            }
-
-            byte[] writeData = { 0 };
-            while (running)
-            {
-                try
-                {
-                    string message = _serialPort.ReadLine();
-                    Console.Error.WriteLine(message);
-                }
-                catch (TimeoutException)
-                {
-                    Console.Error.WriteLine("Nothing to read");
-                    writeData[0] ^= 1;
-                    _serialPort.Write(writeData, 0, 1);
-                }
+                Console.Error.WriteLine("Nothing to read");
+                writeData[0] ^= 1;
+                _serialPort.Write(writeData, 0, 1);
             }
         }
 
-        public void Dispose()
+        protected override void OnStop(object? sender, DoWorkEventArgs args)
         {
-            running = false;
-            readThread.Join();
             _serialPort.Close();
         }
     }
