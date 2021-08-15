29251a2131df0cd8dc2e4f3d7acbc79bbb3da7e0
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-08-15 10:16:26 +0100

    Better logging for pot.

--- Controlzmo/Serial/Pot.cs
+++ Controlzmo/Serial/Pot.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Controlzmo.Hubs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using SimConnectzmo;
 
 namespace Controlzmo.Serial
@@ -7,11 +9,18 @@ namespace Controlzmo.Serial
     [Component]
     public class Pot : ISettable<Int16?>
     {
+        private readonly ILogger _logger;
+
+        public Pot(IServiceProvider sp)
+        {
+            _logger = sp.GetRequiredService<ILogger<Pot>>();
+        }
+
         public string GetId() => "pot";
 
         public void SetInSim(ExtendedSimConnect simConnect, Int16? value)
         {
-            Console.Error.WriteLine($"Imaginary 'pot' set to {value}");
+            _logger.LogTrace($"Imaginary 'pot' set to {value}");
         }
     }
 }
