243e96bfb7c03ce0f919912eab6623b522e17c72
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-08-16 16:41:17 +0100

    Make Serial pot operate speedbrake handle.

--- Controlzmo/Serial/SpeedbrakeHandle.cs
+++ Controlzmo/Serial/SpeedbrakeHandle.cs
@@ -1,26 +1,33 @@
 ﻿using System;
 using Controlzmo.Hubs;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using SimConnectzmo;
+using Controlzmo.Systems.Spoilers;
 
 namespace Controlzmo.Serial
 {
     [Component]
     public class SpeedbrakeHandle : ISettable<Int16?>
     {
-        private readonly ILogger _logger;
+        private readonly IEvent armOn;
+        private readonly IEvent armOff;
+        private readonly IEvent set;
 
         public SpeedbrakeHandle(IServiceProvider sp)
         {
-            _logger = sp.GetRequiredService<ILogger<SpeedbrakeHandle>>();
+            armOn = sp.GetRequiredService<SpoilerArmOnEvent>();
+            armOff = sp.GetRequiredService<SpoilerArmOffEvent>();
+            set = sp.GetRequiredService<SetSpoilerHandleEvent>();
         }
 
         public string GetId() => "speedBrakeHandle";
 
         public void SetInSim(ExtendedSimConnect simConnect, Int16? value)
         {
-            _logger.LogTrace($"Imaginary 'speedBrakeHandle' set to {value}");
+            simConnect.SendEvent(value >= 0 ? armOff : armOn);
+            uint eventData = (uint)Math.Max((int)value!, 0) * 164;
+            eventData = Math.Min(eventData, 16384u);
+            simConnect.SendEvent(set, eventData);
         }
     }
 }
