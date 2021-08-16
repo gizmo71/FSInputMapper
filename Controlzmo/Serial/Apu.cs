3dd35184151070a368096ea45aaeaac6aeda6133
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-08-16 16:23:39 +0100

    Make Serial buttons operate APU.

--- Controlzmo/Serial/Apu.cs
+++ Controlzmo/Serial/Apu.cs
@@ -1,44 +1,44 @@
 ﻿using System;
 using Controlzmo.Hubs;
+using Controlzmo.Systems.JetBridge;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using SimConnectzmo;
 
 namespace Controlzmo.Serial
 {
     [Component]
-    public class ApuMasterButton : ISettable<Int16?>
+    public class ApuMasterButton : ISettable<bool?>
     {
-        private readonly ILogger _logger;
+        private readonly JetBridgeSender sender;
 
         public ApuMasterButton(IServiceProvider sp)
         {
-            _logger = sp.GetRequiredService<ILogger<ApuMasterButton>>();
+            sender = sp.GetRequiredService<JetBridgeSender>();
         }
 
         public string GetId() => "apuMasterPressed";
 
-        public void SetInSim(ExtendedSimConnect simConnect, Int16? value)
+        public void SetInSim(ExtendedSimConnect simConnect, bool? value)
         {
-            _logger.LogTrace($"APU master button pressed");
+            sender.Execute(simConnect, $"1 (L:A32NX_OVHD_APU_MASTER_SW_PB_IS_ON, Bool) - (>L:A32NX_OVHD_APU_MASTER_SW_PB_IS_ON)");
         }
     }
 
     [Component]
-    public class ApuStartButton : ISettable<Int16?>
+    public class ApuStartButton : ISettable<bool?>
     {
-        private readonly ILogger _logger;
+        private readonly JetBridgeSender sender;
 
         public ApuStartButton(IServiceProvider sp)
         {
-            _logger = sp.GetRequiredService<ILogger<ApuMasterButton>>();
+            sender = sp.GetRequiredService<JetBridgeSender>();
         }
 
         public string GetId() => "apuStartPressed";
 
-        public void SetInSim(ExtendedSimConnect simConnect, Int16? value)
+        public void SetInSim(ExtendedSimConnect simConnect, bool? value)
         {
-            _logger.LogTrace($"APU start button pressed");
+            sender.Execute(simConnect, "(L:A32NX_OVHD_APU_START_PB_IS_AVAILABLE, Bool) if { 1 (>L:A32NX_OVHD_APU_START_PB_IS_ON, Bool) }");
         }
     }
 }
