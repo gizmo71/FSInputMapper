ae9dcd990387e148b558e0dc2bcb72bfed284555
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-09-26 15:23:40 +0100

    Untested EFIS mode.

--- Controlzmo/Systems/EfisControlPanel/EfisMode.cs
+++ Controlzmo/Systems/EfisControlPanel/EfisMode.cs
@@ -1,23 +1,56 @@
-/*
+using Controlzmo.Hubs;
+using Controlzmo.Systems.JetBridge;
+using SimConnectzmo;
+using System;
+using System.Numerics;
 
-A32NX_EFIS_{side}_ND_MODE
+namespace Controlzmo.Systems.EfisControlPanel
+{
+    public abstract class EfisMode : ISettable<string>
+    {
+        protected readonly JetBridgeSender sender;
+        protected readonly string id;
+        protected readonly string lvarName;
 
-    Enum
-    Provides the selected navigation display mode for the captain side EFIS
-    Value 	Meaning
-    0 	ROSE ILS
-    1 	ROSE VOR
-    2 	ROSE NAV
-    3 	ARC
-    4 	PLAN
-    {side}
-        L
-        R
+        protected EfisMode(JetBridgeSender sender, string side)
+        {
+            this.sender = sender;
+            id = $"{side}EfisNdMode";
+            var sideCode = side.Substring(0, 1).ToUpper();
+            lvarName = $"A32NX_EFIS_{sideCode}_ND_MODE";
+        }
 
-The navaid switches can be read/set using L:A32NX_EFIS_L/R_NAVAID_1/2_MODE (0 off, 1 ADF, 2 VOR).
-The five option buttons can be read/set with L:A32NX_EFIS_L/R_OPTION (0 none, 1 CSTR, 3 WPT, 2 VORD, 4 NDB, 5 ARPT).
+        public string GetId() => id;
 
-The LS pushbuttons under Baro are BTN_LS_1/2_FILTER_ACTIVE (read/write).
-Where are the FD buttons?
+        public void SetInSim(ExtendedSimConnect simConnect, string? label)
+        {
+            var code = label switch
+            {
+                "Rose ILS" => 0,
+                "Rose VOR" => 1,
+                "Rose Nav" => 2,
+                "Arc" => 3,
+                "Plan" => 4,
+                _ => throw new ArgumentOutOfRangeException($"Unrecognised EFIS mode '{label}'")
+            };
+            sender.Execute(simConnect, $"{code} (>L:{lvarName})");
+        }
+    }
 
-*/
+    [Component]
+    public class EfisLeftMode : EfisMode
+    {
+        public EfisLeftMode(JetBridgeSender sender) : base(sender, "left") { }
+    }
+
+    [Component]
+    public class EfisRightMode : EfisMode
+    {
+        public EfisRightMode(JetBridgeSender sender) : base(sender, "right") { }
+    }
+}
+    /*The navaid switches can be read/set using L:A32NX_EFIS_L/R_NAVAID_1/2_MODE (0 off, 1 ADF, 2 VOR).
+    The five option buttons can be read/set with L:A32NX_EFIS_L/R_OPTION (0 none, 1 CSTR, 3 WPT, 2 VORD, 4 NDB, 5 ARPT).
+
+    The LS pushbuttons under Baro are BTN_LS_1/2_FILTER_ACTIVE (read/write).
+    Where are the FD buttons?    */
