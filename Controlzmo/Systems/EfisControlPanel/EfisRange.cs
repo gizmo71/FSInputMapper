8d8dd99e86a197f0e44d0fa7d65fb4b780f3a137
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-09-26 15:06:52 +0100

    Untested implementation of EFIS range.

--- Controlzmo/Systems/EfisControlPanel/EfisRange.cs
+++ Controlzmo/Systems/EfisControlPanel/EfisRange.cs
@@ -1,18 +1,44 @@
-/*
+using Controlzmo.Hubs;
+using Controlzmo.Systems.JetBridge;
+using SimConnectzmo;
+using System;
+using System.Numerics;
 
-A32NX_EFIS_{side}_ND_RANGE
+namespace Controlzmo.Systems.EfisControlPanel
+{
+    public abstract class EfisRange : ISettable<string>
+    {
+        protected readonly JetBridgeSender sender;
+        protected readonly string id;
+        protected readonly string lvarName;
 
-    Enum
-    Provides the selected navigation display range for the captain side EFIS
-    Value 	Meaning
-    0 	10
-    1 	20
-    2 	40
-    3 	80
-    4 	160
-    5 	320
-    {side}
-        L
-        R
+        protected EfisRange(JetBridgeSender sender, string side)
+        {
+            this.sender = sender;
+            id = $"{side}EfisNdRange";
+            var sideCode = side.Substring(0, 1).ToUpper();
+            lvarName = $"A32NX_EFIS_{sideCode}_ND_RANGE";
+        }
 
-*/
+        public string GetId() => id;
+
+        public void SetInSim(ExtendedSimConnect simConnect, string? label)
+        {
+            UInt32 range = UInt32.Parse(label!);
+            var code = Math.Clamp(BitOperations.Log2(range / 10), 0, 5);
+            sender.Execute(simConnect, $"{code} (>L:{lvarName})");
+        }
+    }
+
+    [Component]
+    public class EfisLeftRange : EfisRange
+    {
+        public EfisLeftRange(JetBridgeSender sender) : base(sender, "left") { }
+    }
+
+    [Component]
+    public class EfisRightRange : EfisRange
+    {
+        public EfisRightRange(JetBridgeSender sender) : base(sender, "right") { }
+    }
+}
