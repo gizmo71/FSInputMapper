f15c7cab44f94c4e01cd590cee1731f692ee3295
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-09-27 20:50:01 +0100

    Add EFIS Nav Aid switches.

--- Controlzmo/README.md
+++ Controlzmo/README.md
@@ -31,8 +31,6 @@ https://docs.microsoft.com/en-us/previous-versions/microsoft-esp/cc526953(v=msdn
 EFIS
 ----
 
-The navaid switches can be read/set using L:A32NX_EFIS_L/R_NAVAID_1/2_MODE (0 off, 1 ADF, 2 VOR).
-
 The five option buttons can be read/set with L:A32NX_EFIS_L/R_OPTION (0 none, 1 CSTR, 3 WPT, 2 VORD, 4 NDB, 5 ARPT).
 
 The LS pushbuttons under Baro are BTN_LS_1/2_FILTER_ACTIVE (read/write).
---
+++ Controlzmo/Systems/EfisControlPanel/EfisNavAids.cs
@@ -0,0 +1,60 @@
+﻿using Controlzmo.Hubs;
+using Controlzmo.Systems.JetBridge;
+using SimConnectzmo;
+using System;
+
+namespace Controlzmo.Systems.EfisControlPanel
+{
+    public abstract class EfisNavAid : ISettable<string>
+    {
+        protected readonly JetBridgeSender sender;
+        protected readonly string id;
+        protected readonly string lvarName;
+
+        protected EfisNavAid(JetBridgeSender sender, string side, int number)
+        {
+            this.sender = sender;
+            id = $"{side}EfisNavAid{number}";
+            var sideCode = side.Substring(0, 1).ToUpper();
+            lvarName = $"A32NX_EFIS_{sideCode}_NAVAID_{number}_MODE";
+        }
+
+        public string GetId() => id;
+
+        public void SetInSim(ExtendedSimConnect simConnect, string? label)
+        {
+            var code = label switch
+            {
+                "Off" => 0,
+                "ADF" => 1,
+                "VOR" => 2,
+                _ => throw new ArgumentOutOfRangeException($"Unrecognised EFIS navaid setting '{label}'")
+            };
+            sender.Execute(simConnect, $"{code} (>L:{lvarName})");
+        }
+    }
+
+    [Component]
+    public class EfisLeftNavAid1 : EfisNavAid
+    {
+        public EfisLeftNavAid1(JetBridgeSender sender) : base(sender, "left", 1) { }
+    }
+
+    [Component]
+    public class EfisLeftNavAid2 : EfisNavAid
+    {
+        public EfisLeftNavAid2(JetBridgeSender sender) : base(sender, "left", 2) { }
+    }
+
+    [Component]
+    public class EfisRightNavAid1 : EfisNavAid
+    {
+        public EfisRightNavAid1(JetBridgeSender sender) : base(sender, "right", 1) { }
+    }
+
+    [Component]
+    public class EfisRightNavAid2 : EfisNavAid
+    {
+        public EfisRightNavAid2(JetBridgeSender sender) : base(sender, "right", 2) { }
+    }
+}
