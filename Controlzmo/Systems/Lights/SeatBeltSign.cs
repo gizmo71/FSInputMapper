b558526d15f347ead3bb171c559e4bf940bd839e
Dave Gymer <gizmo@davegymer.org>
2021-10-07 16:07:31 +0100

    Fix seat belt sign to work the correct way round, and be a component.

--- Controlzmo/Systems/Lights/SeatBeltSign.cs
+++ Controlzmo/Systems/Lights/SeatBeltSign.cs
@@ -1,15 +1,12 @@
 ﻿using Controlzmo.Hubs;
 using Controlzmo.Systems.JetBridge;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
 using System;
-using System.Runtime.InteropServices;
 
 namespace Controlzmo.Systems.Lights
 {
+    [Component]
     public class SeatBeltSign : ISettable<bool?>
     {
         private readonly JetBridgeSender sender;
@@ -21,7 +18,7 @@ namespace Controlzmo.Systems.Lights
         public void SetInSim(ExtendedSimConnect simConnect, bool? value)
         {
             var desiredValue = value == true ? 1 : 0;
-            sender.Execute(simConnect, $"(A:CABIN SEATBELTS ALERT SWITCH,Bool) {desiredValue} =="
+            sender.Execute(simConnect, $"(A:CABIN SEATBELTS ALERT SWITCH,Bool) {desiredValue} !="
                 + " if{{ (> K:CABIN_SEATBELTS_ALERT_SWITCH_TOGGLE) }}");
         }
     }
