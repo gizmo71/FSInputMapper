8d9cd76bc233cf4d094058b8e5a6fad249e4e5f1
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-10-08 19:03:12 +0100

    Allow eternal parties to set the temporary displays.

---
+++ Controlzmo/Serial/TempDisplays.cs
@@ -0,0 +1,62 @@
+﻿using Controlzmo.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using SimConnectzmo;
+using System;
+
+namespace Controlzmo.Serial
+{
+    [Component]
+    public class BaroDisplay : ISettable<string>
+    {
+        private readonly IHubContext<ControlzmoHub, IControlzmoHub> hub;
+
+        public BaroDisplay(IServiceProvider sp)
+        {
+            hub = sp.GetRequiredService<IHubContext<ControlzmoHub, IControlzmoHub>>();
+        }
+
+        public string GetId() => "baroDisplay";
+
+        public void SetInSim(ExtendedSimConnect simConnect, string? value)
+        {
+            hub.Clients.All.SetFromSim(GetId(), value);
+        }
+    }
+
+    [Component]
+    public class FcuDisplayLeft : ISettable<string>
+    {
+        private readonly IHubContext<ControlzmoHub, IControlzmoHub> hub;
+
+        public FcuDisplayLeft(IServiceProvider sp)
+        {
+            hub = sp.GetRequiredService<IHubContext<ControlzmoHub, IControlzmoHub>>();
+        }
+
+        public string GetId() => "fcuDisplayLeft";
+
+        public void SetInSim(ExtendedSimConnect simConnect, string? value)
+        {
+            hub.Clients.All.SetFromSim(GetId(), value);
+        }
+    }
+
+    [Component]
+    public class FcuDisplayRight : ISettable<string>
+    {
+        private readonly IHubContext<ControlzmoHub, IControlzmoHub> hub;
+
+        public FcuDisplayRight(IServiceProvider sp)
+        {
+            hub = sp.GetRequiredService<IHubContext<ControlzmoHub, IControlzmoHub>>();
+        }
+
+        public string GetId() => "fcuDisplayRight";
+
+        public void SetInSim(ExtendedSimConnect simConnect, string? value)
+        {
+            hub.Clients.All.SetFromSim(GetId(), value);
+        }
+    }
+}
