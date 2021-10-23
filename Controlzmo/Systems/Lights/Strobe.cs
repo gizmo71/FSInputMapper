f2259b324f3da3570f28bb1ee80a3b81a09d2e33
Dave Gymer <gizmo@davegymer.org>
2021-10-23 11:23:12 +0100

    Strobe light LVar usage has changed again!

--- Controlzmo/Systems/Lights/Strobe.cs
+++ Controlzmo/Systems/Lights/Strobe.cs
@@ -9,32 +9,23 @@ using SimConnectzmo;
 namespace Controlzmo.Systems.Lights
 {
     [Component]
-    public class StrobeLightSystem : LVar, IOnSimConnection, ISettable<string?>
+    public class StrobeLightSystem : ISettable<string?>
     {
         private readonly IHubContext<ControlzmoHub, IControlzmoHub> hub;
         private readonly JetBridgeSender sender;
 
-        public StrobeLightSystem(IServiceProvider sp) : base(sp)
+        public StrobeLightSystem(IServiceProvider sp)
         {
             hub = sp.GetRequiredService<IHubContext<ControlzmoHub, IControlzmoHub>>();
             sender = sp.GetRequiredService<JetBridgeSender>();
         }
 
-        protected override string LVarName() => "LIGHTING_STROBE_0";
-        protected override int Milliseconds() => 1000;
-        protected override double Default() => -1;
-
-        public void OnConnection(ExtendedSimConnect simConnect) => Request(simConnect);
-
-        protected override double? Value { set => hub.Clients.All.SetFromSim(GetId(), (base.Value = value) switch { 0 => "on", 2 => "off", 1 => "auto", _ => null }); }
-
         public string GetId() => "lightsStrobe";
 
-        public void SetInSim(ExtendedSimConnect simConnect, string? value)
+        public void SetInSim(ExtendedSimConnect simConnect, string? position)
         {
-            var auto = value == "auto" ? 1 : 0;
-            var set = value == "off" ? 0 : 1;
-            sender.Execute(simConnect, $"{auto} (>L:STROBE_0_Auto) {set} (>K:STROBES_SET)");
+            var value = position switch { "off" => 2, "on" => 0, "auto" => 1, _ => throw new ArgumentException($"Unknown strobe position ${position}") };
+            sender.Execute(simConnect, $"{value} (>L:LIGHTING_STROBE_0)");
         }
     }
 }
