0797376f65c2fd45ab8173748a213fce5eacefdb
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-11-07 18:38:01 +0000

    Add note about IoBounce usage.

--- Picozmo/IoBounce2.h
+++ Picozmo/IoBounce2.h
@@ -5,6 +5,7 @@ class IoBounce : public Bounce {
   IoAbstractionRef ioAbstraction;
 public:
   IoBounce(IoAbstractionRef ioAbstraction) : Bounce(), ioAbstraction(ioAbstraction) { }
+  // Don't call these, they just join the Bounce to the IoAbstraction.
   bool readCurrentState() { return ioAbstraction->readValue(pin); }
   void setPinMode(int pin, int mode) { ioAbstraction->pinDirection(pin, mode); }
 };
