8326ce39abdd84c402fe6fb5449bf2f38e3396e8
Dave Gymer <gizmo@davegymer.org>
2023-05-10 17:21:51 +0100

    Load SOPs from an XML on GitHub.

--- Controlzmo/Systems/PilotMonitoring/AtcAirline.cs
+++ Controlzmo/Systems/PilotMonitoring/AtcAirline.cs
@@ -1,9 +1,14 @@
 ﻿using Controlzmo.Hubs;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Runtime.InteropServices;
-using Microsoft.AspNetCore.SignalR;
-using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
 
 namespace Controlzmo.Systems.PilotMonitoring
 {
@@ -33,17 +38,99 @@ namespace Controlzmo.Systems.PilotMonitoring
             simConnect.RequestDataOnSimObject(this, SIMCONNECT_PERIOD.SECOND);
         }
 
-        private readonly Regex vbaw = new Regex(@"^(speedbird|shuttle)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private readonly HttpClient client = new HttpClient();
 
         public override void Process(ExtendedSimConnect simConnect, AtcAirlineData data)
         {
-            var sops = $"No SOPs available for {data.model} with callsign {data.name}";
-            if (vbaw.IsMatch(data.name))
+            var icaoCode = data.model.ToUpper();
+            var callsign = data.name.ToLower();
+            var sops = $"No SOPs available for {icaoCode} with callsign {callsign}";
+            try
             {
-                if (data.model == "A20N")
-                    sops = "Start #2 first; warm up, cool down\nFlight time must be within 20%/20 minutes of scheduled";
+                var doc = new XmlDocument();
+                doc.Load("https://github.com/gizmo71/FSInputMapper/raw/master/Controlzmo/SOPs.xml");
+                var context = new VariableContext { { "callsign", callsign }, { "icaoType", icaoCode } };
+                var node = doc.DocumentElement?.SelectSingleNode($"//Airline[./Callsign/@of = $callsign]/Type[@icao=$icaoType]/Text", context);
+                if (node != null)
+                    sops = node.InnerText;
+            }
+            catch (Exception e)
+            {
+                sops = e.ToString();
             }
             hub.Clients.All.SetFromSim("atcAirline", sops);
         }
+    }
+
+    // From https://stackoverflow.com/questions/19498192/xpath-injection-mitigation/19704008#19704008
+    class VariableContext : XsltContext
+    {
+        private Dictionary<string, object> m_values;
+
+        public VariableContext()
+        {
+            m_values = new Dictionary<string, object>();
+        }
+
+        public void Add(string name, object value)
+        {
+            m_values[name] = value;
+        }
+
+        public override IXsltContextVariable ResolveVariable(string prefix, string name)
+        {
+            return new XPathVariable(m_values[name]);
+        }
+
+        public override int CompareDocument(string baseUri, string nextbaseUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool PreserveWhitespace(XPathNavigator node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IXsltContextFunction ResolveFunction(string prefix, string name, XPathResultType[] ArgTypes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Whitespace
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        private class XPathVariable : IXsltContextVariable
+        {
+            private object m_value;
+
+            internal XPathVariable(object value)
+            {
+                m_value = value;
+            }
+
+            public object Evaluate(XsltContext xsltContext)
+            {
+                return m_value;
+            }
+
+            public bool IsLocal
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public bool IsParam
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public XPathResultType VariableType
+            {
+                get { throw new NotImplementedException(); }
+            }
+        }
+
     }
 }
