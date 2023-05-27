02e44ee19dd95c01701e5e82e18642f4310db8ce
Dave Gymer <gizmo@davegymer.org>
2023-05-27 11:22:00 +0100

    Convert Thrust Set to LVars via simconnect.

--- Controlzmo/Systems/PilotMonitoring/Engine.cs
+++ Controlzmo/Systems/PilotMonitoring/Engine.cs
@@ -1,12 +1,6 @@
-﻿using Controlzmo.Hubs;
-using Controlzmo.SimConnectzmo;
-using Microsoft.AspNetCore.SignalR;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.FlightSimulator.SimConnect;
-using SimConnectzmo;
+﻿using Controlzmo.SimConnectzmo;
 using System;
 using System.ComponentModel;
-using System.Runtime.InteropServices;
 
 namespace Controlzmo.Systems.PilotMonitoring
 {
@@ -34,57 +28,5 @@ namespace Controlzmo.Systems.PilotMonitoring
     public class Engine2State : EngineState
     {
         public Engine2State(IServiceProvider serviceProvider) : base(serviceProvider, 2) { }
-    }
-
-    public abstract class EngineN1 : SwitchableLVar
-    {
-        private readonly string lVarName;
-
-        public EngineN1(IServiceProvider serviceProvider, int engineNumber) : base(serviceProvider)
-        {
-            lVarName = "A32NX_ENGINE_N1:" + engineNumber;
-        }
-
-        protected override string LVarName() => lVarName;
-        protected override double Default() => -1.0;
-    }
-
-    [Component]
-    public class Engine1N1 : EngineN1
-    {
-        public Engine1N1(IServiceProvider serviceProvider) : base(serviceProvider, 1) { }
-    }
-
-    [Component]
-    public class Engine2N1 : EngineN1
-    {
-        public Engine2N1(IServiceProvider serviceProvider) : base(serviceProvider, 2) { }
-    }
-
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
-    public struct EngineN1Data
-    {
-        [SimVar("L:A32NX_ENGINE_N1:1", "number", SIMCONNECT_DATATYPE.FLOAT64, 0.1f)]
-        public Double engine1N1;
-        [SimVar("L:A32NX_ENGINE_N1:2", "number", SIMCONNECT_DATATYPE.FLOAT64, 0.1f)]
-        public Double engine2N1;
-    };
-
-    [Component]
-    public class EngineN1Listener : DataListener<EngineN1Data>, IRequestDataOnOpen
-    {
-        private readonly IHubContext<ControlzmoHub, IControlzmoHub> hubContext;
-
-        public EngineN1Listener(IServiceProvider serviceProvider)
-        {
-            hubContext = serviceProvider.GetRequiredService<IHubContext<ControlzmoHub, IControlzmoHub>>();
-        }
-
-        public SIMCONNECT_PERIOD GetInitialRequestPeriod() => SIMCONNECT_PERIOD.SECOND;
-
-        public override void Process(ExtendedSimConnect simConnect, EngineN1Data data)
-        {
-            //TODO: replace the stuff in ThrustSet with traditional data listeners.
-        }
     }
 }
--- Controlzmo/Systems/PilotMonitoring/ThrustSet.cs
+++ Controlzmo/Systems/PilotMonitoring/ThrustSet.cs
@@ -1,91 +1,52 @@
-﻿using System;
-using System.ComponentModel;
-using Controlzmo.Hubs;
-using Controlzmo.SimConnectzmo;
+﻿using Controlzmo.Hubs;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 namespace Controlzmo.Systems.PilotMonitoring
 {
-    public abstract class SwitchableLVar : LVar
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct ThrustSetData
     {
-        public SwitchableLVar(IServiceProvider serviceProvider) : base(serviceProvider) { }
-        protected override int Milliseconds() => 4000;
-    }
+        [SimVar("L:A32NX_AUTOTHRUST_TLA_N1:1", "number", SIMCONNECT_DATATYPE.FLOAT64, 0.1f)]
+        public Double tla1;
+        [SimVar("L:A32NX_AUTOTHRUST_TLA_N1:2", "number", SIMCONNECT_DATATYPE.FLOAT64, 0.1f)]
+        public Double tla2;
+        [SimVar("L:A32NX_ENGINE_N1:1", "number", SIMCONNECT_DATATYPE.FLOAT64, 0.1f)]
+        public Double engine1N1;
+        [SimVar("L:A32NX_ENGINE_N1:2", "number", SIMCONNECT_DATATYPE.FLOAT64, 0.1f)]
+        public Double engine2N1;
+        [SimVar("L:A32NX_AUTOTHRUST_MODE", "number", SIMCONNECT_DATATYPE.INT32, 1.0f)]
+        public Int32 autothrustMode;
+    };
 
     [Component]
-    public class AutothrustMode : SwitchableLVar, IOnSimConnection
+    public class ThrustSetListener : DataListener<ThrustSetData>
     {
-        private readonly ThrustLeverN1 thrustLever1N1;
-        private readonly ThrustLeverN1 thrustLever2N1;
-        private readonly EngineN1 engine1N1;
-        private readonly EngineN1 engine2N1;
-
-        private readonly SimConnectHolder scHolder;
         private readonly IHubContext<ControlzmoHub, IControlzmoHub> hubContext;
 
-        public AutothrustMode(IServiceProvider serviceProvider) : base(serviceProvider)
+        public ThrustSetListener(IServiceProvider serviceProvider)
         {
-            thrustLever1N1 = serviceProvider.GetRequiredService<ThrustLever1N1>();
-            thrustLever2N1 = serviceProvider.GetRequiredService<ThrustLever2N1>();
-            engine1N1 = serviceProvider.GetRequiredService<Engine1N1>();
-            engine2N1 = serviceProvider.GetRequiredService<Engine2N1>();
-
-            engine1N1.PropertyChanged += OnPropertyChanged;
-            engine2N1.PropertyChanged += OnPropertyChanged;
-
-            scHolder = serviceProvider.GetRequiredService<SimConnectHolder>();
             hubContext = serviceProvider.GetRequiredService<IHubContext<ControlzmoHub, IControlzmoHub>>();
-
             serviceProvider.GetRequiredService<RunwayCallsStateListener>().onGroundHandlers += OnGroundHandler;
         }
 
-        protected override string LVarName() => "A32NX_AUTOTHRUST_MODE";
-        protected override double Default() => -1.0;
-
         private void OnGroundHandler(ExtendedSimConnect simConnect, bool isOnGround)
         {
-            Request(simConnect, isOnGround ? 1000 : 0);
-            if (!isOnGround) {
-                engine1N1.Request(simConnect, 0);
-                engine2N1.Request(simConnect, 0);
-            }
+            simConnect.RequestDataOnSimObject(this, isOnGround ? SIMCONNECT_CLIENT_DATA_PERIOD.SECOND : SIMCONNECT_CLIENT_DATA_PERIOD.NEVER);
         }
-
-        public void OnConnection(ExtendedSimConnect simConnect)
-        {
-            // Do nothing, but get me created. This does not seem right compared to other things.
-        }
-
-        protected override double? Value
-        {
-            set
-            {
-                if (base.Value != value)
-                {
-                    base.Value = value;
-                    var sc = scHolder.SimConnect!;
-                    thrustLever1N1.Request(sc);
-                    thrustLever2N1.Request(sc);
-                    int period = IsTakeOffPower() ? 1000 : 0;
-                    engine1N1.Request(sc, period);
-                    engine2N1.Request(sc, period);
-                }
-            }
-        }
-
-        private const double TOGA = 1;
-        private const double FLEX = 3;
-        private Boolean IsTakeOffPower() => Value == TOGA || Value == FLEX;
 
         private Boolean isCalled = false;
 
-        private void OnPropertyChanged(object? sender, PropertyChangedEventArgs args)
+        public override void Process(ExtendedSimConnect simConnect, ThrustSetData data)
         {
-            if (IsTakeOffPower())
+            if (IsTakeOffPower(data))
             {
-                if (!isCalled && isSet(engine1N1, thrustLever1N1) && isSet(engine2N1, thrustLever2N1))
+                if (!isCalled && IsSet(data.engine1N1, data.tla1) && IsSet(data.engine2N1, data.tla2))
                 {
                     hubContext.Clients.All.Speak($"thrust set");
                     isCalled = true;
@@ -95,9 +56,10 @@ namespace Controlzmo.Systems.PilotMonitoring
                 isCalled = false;
         }
 
-        private Boolean isSet(EngineN1 engine, ThrustLeverN1 lever)
-        {
-            return lever > 75.0 && engine >= lever - 0.1;
-        }
+        private const Int32 TOGA = 1;
+        private const Int32 FLEX = 3;
+        private static Boolean IsTakeOffPower(ThrustSetData data) => data.autothrustMode == TOGA || data.autothrustMode == FLEX;
+
+        private static Boolean IsSet(Double engine, Double lever) => lever > 75.0 && engine >= lever - 0.1;
     }
 }
