40c1efd182c9253dcbb433f9b95619d4e6860fae
Dave Gymer <gizmo@davegymer.org>
2023-07-01 10:46:29 +0100

    Convert Brakes Hot to new LVar style. Untested.

--- Controlzmo/Systems/PilotMonitoring/BrakesHot.cs
+++ Controlzmo/Systems/PilotMonitoring/BrakesHot.cs
@@ -1,31 +1,36 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Controlzmo.Hubs;
-using Controlzmo.SimConnectzmo;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
 
 namespace Controlzmo.Systems.PilotMonitoring
 {
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct BrakesHotData
+    {
+        [SimVar("L:A32NX_BRAKES_HOT", "bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
+        public Int32 areBrakesHot;
+    };
+
     [Component]
-    public class BrakesHot : LVar, IOnSimConnection
+    public class BrakesHot : DataListener<BrakesHotData>, IOnSimStarted
     {
         private readonly IHubContext<ControlzmoHub, IControlzmoHub> hubContext;
 
-        public BrakesHot(IServiceProvider serviceProvider) : base(serviceProvider)
+        public BrakesHot(IServiceProvider serviceProvider)
         {
             hubContext = serviceProvider.GetRequiredService<IHubContext<ControlzmoHub, IControlzmoHub>>();
         }
 
-        protected override string LVarName() => "A32NX_BRAKES_HOT";
-        protected override int Milliseconds() => 4000;
-        protected override double Default() => -1.0;
+        public void OnStarted(ExtendedSimConnect simConnect) => simConnect.RequestDataOnSimObject(this, SIMCONNECT_PERIOD.SECOND);
 
-        public void OnConnection(ExtendedSimConnect simConnect) => Request(simConnect);
-
-        protected override double? Value
+        public override void Process(ExtendedSimConnect simConnect, BrakesHotData data)
         {
-            set { if ((base.Value = value) == 1.0) hubContext.Clients.All.Speak("Brakes hot"); }
+            if (data.areBrakesHot == 1)
+                hubContext.Clients.All.Speak("Brakes hot");
         }
     }
 }
