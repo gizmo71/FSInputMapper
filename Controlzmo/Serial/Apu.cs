38da9f98597fa7987aff151f06c31fd855976f2e
Dave Gymer <gizmo@davegymer.org>
2023-07-01 10:42:06 +0100

    Convert APU code to new LVar style.

--- Controlzmo/Serial/Apu.cs
+++ Controlzmo/Serial/Apu.cs
@@ -1,60 +1,54 @@
 ﻿using Controlzmo.Hubs;
-using Controlzmo.SimConnectzmo;
 using Controlzmo.Systems.JetBridge;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
 using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 // Bleed button states: `L:A32NX_OVHD_PNEU_APU_BLEED_PB_IS_ON`, `L:A32NX_OVHD_PNEU_APU_BLEED_PB_HAS_FAULT`
 // Toggle bleed with 0/`1 (>L:A32NX_OVHD_PNEU_APU_BLEED_PB_IS_ON`) Bool; also `L:A32NX_OVHD_PNEU_APU_BLEED_PB_HAS_FAULT`
 namespace Controlzmo.Serial
 {
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct ApuFaultData
+    {
+        [SimVar("L:A32NX_OVHD_APU_MASTER_SW_PB_HAS_FAULT", "bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
+        public Int32 isApuFault;
+    };
+
     [Component]
-    public class ApuFault : LVar, IOnSimStarted
+    public class ApuFault : DataListener<ApuFaultData>, IOnSimStarted
     {
         private readonly SerialPico serial;
 
-        public ApuFault(IServiceProvider serviceProvider) : base(serviceProvider)
-        {
-            serial = serviceProvider.GetRequiredService<SerialPico>();
-        }
-
-        protected override string LVarName() => "A32NX_OVHD_APU_MASTER_SW_PB_HAS_FAULT";
-        public void OnStarted(ExtendedSimConnect simConnect) => Request(simConnect);
-
-        protected override double? Value { set { base.Value = value; send(); } }
-
-        private void send() => serial.SendLine("ApuFault=" + (Value == 1));
+        public ApuFault(IServiceProvider serviceProvider) => serial = serviceProvider.GetRequiredService<SerialPico>();
+        public void OnStarted(ExtendedSimConnect simConnect) => simConnect.RequestDataOnSimObject(this, SIMCONNECT_PERIOD.VISUAL_FRAME);
+        public override void Process(ExtendedSimConnect simConnect, ApuFaultData data) => serial.SendLine("ApuFault=" + (data.isApuFault == 1));
     }
 
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct ApuMasterData
+    {
+        [SimVar("L:A32NX_OVHD_APU_MASTER_SW_PB_IS_ON", "bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
+        public Int32 isApuMasterOn;
+    };
+
     [Component]
-    public class ApuMasterOn : LVar, IOnSimStarted
+    public class ApuMasterOn : DataListener<ApuMasterData>, IOnSimStarted
     {
         private readonly SerialPico serial;
-
-        public ApuMasterOn(IServiceProvider serviceProvider) : base(serviceProvider)
-        {
-            serial = serviceProvider.GetRequiredService<SerialPico>();
-        }
-
-        protected override string LVarName() => "A32NX_OVHD_APU_MASTER_SW_PB_IS_ON";
-        public void OnStarted(ExtendedSimConnect simConnect) => Request(simConnect);
-
-        protected override double? Value { set { base.Value = value; send(); } }
-
-        private void send() => serial.SendLine("ApuMasterOn=" + (Value == 1));
+        public ApuMasterOn(IServiceProvider serviceProvider) => serial = serviceProvider.GetRequiredService<SerialPico>();
+        public void OnStarted(ExtendedSimConnect simConnect) => simConnect.RequestDataOnSimObject(this, SIMCONNECT_PERIOD.VISUAL_FRAME);
+        public override void Process(ExtendedSimConnect simConnect, ApuMasterData data) => serial.SendLine("ApuMasterOn=" + (data.isApuMasterOn == 1));
     }
 
     [Component]
     public class ApuMasterButton : ISettable<bool?>
     {
         private readonly JetBridgeSender sender;
-
-        public ApuMasterButton(IServiceProvider sp)
-        {
-            sender = sp.GetRequiredService<JetBridgeSender>();
-        }
-
+        public ApuMasterButton(IServiceProvider sp) => sender = sp.GetRequiredService<JetBridgeSender>();
         public string GetId() => "apuMasterPressed";
 
         public void SetInSim(ExtendedSimConnect simConnect, bool? value)
@@ -63,52 +57,45 @@ namespace Controlzmo.Serial
         }
     }
 
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct ApuAvailData
+    {
+        [SimVar("L:A32NX_OVHD_APU_START_PB_IS_AVAILABLE", "bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
+        public Int32 isApuAvail;
+    };
+
     [Component]
-    public class ApuAvail : LVar, IOnSimStarted
+    public class ApuAvail : DataListener<ApuAvailData>, IOnSimStarted
     {
         private readonly SerialPico serial;
 
-        public ApuAvail(IServiceProvider serviceProvider) : base(serviceProvider)
-        {
-            serial = serviceProvider.GetRequiredService<SerialPico>();
-        }
-
-        protected override string LVarName() => "A32NX_OVHD_APU_START_PB_IS_AVAILABLE";
-        public void OnStarted(ExtendedSimConnect simConnect) => Request(simConnect);
-
-        protected override double? Value { set { base.Value = value; send(); } }
-
-        private void send() => serial.SendLine("ApuAvail=" + (Value == 1));
+        public ApuAvail(IServiceProvider serviceProvider) => serial = serviceProvider.GetRequiredService<SerialPico>();
+        public void OnStarted(ExtendedSimConnect simConnect) => simConnect.RequestDataOnSimObject(this, SIMCONNECT_PERIOD.VISUAL_FRAME);
+        public override void Process(ExtendedSimConnect simConnect, ApuAvailData data) => serial.SendLine("ApuAvail=" + (data.isApuAvail == 1));
     }
 
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct ApuStartOnData
+    {
+        [SimVar("L:A32NX_OVHD_APU_START_PB_IS_ON", "bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
+        public Int32 isApuStartOn;
+    };
+
     [Component]
-    public class ApuStartOn : LVar, IOnSimStarted
+    public class ApuStartOn : DataListener<ApuStartOnData>, IOnSimStarted
     {
         private readonly SerialPico serial;
 
-        public ApuStartOn(IServiceProvider serviceProvider) : base(serviceProvider)
-        {
-            serial = serviceProvider.GetRequiredService<SerialPico>();
-        }
-
-        protected override string LVarName() => "A32NX_OVHD_APU_START_PB_IS_ON";
-        public void OnStarted(ExtendedSimConnect simConnect) => Request(simConnect);
-
-        protected override double? Value { set { base.Value = value; send(); } }
-
-        private void send() => serial.SendLine("ApuStartOn=" + (Value == 1));
+        public ApuStartOn(IServiceProvider serviceProvider) => serial = serviceProvider.GetRequiredService<SerialPico>();
+        public void OnStarted(ExtendedSimConnect simConnect) => simConnect.RequestDataOnSimObject(this, SIMCONNECT_PERIOD.VISUAL_FRAME);
+        public override void Process(ExtendedSimConnect simConnect, ApuStartOnData data) => serial.SendLine("ApuStartOn=" + (data.isApuStartOn == 1));
     }
 
     [Component]
     public class ApuStartButton : ISettable<bool?>
     {
         private readonly JetBridgeSender sender;
-
-        public ApuStartButton(IServiceProvider sp)
-        {
-            sender = sp.GetRequiredService<JetBridgeSender>();
-        }
-
+        public ApuStartButton(IServiceProvider sp) => sender = sp.GetRequiredService<JetBridgeSender>();
         public string GetId() => "apuStartPressed";
 
         public void SetInSim(ExtendedSimConnect simConnect, bool? value)
