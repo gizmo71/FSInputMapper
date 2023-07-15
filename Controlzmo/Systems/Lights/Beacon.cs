95bfa100a5d34e3f7bb41f68a7cfa46e1f4305de
Dave Gymer <gizmo@davegymer.org>
2023-07-15 08:37:27 +0100

    Use newer event for beacon.

--- Controlzmo/Systems/Lights/Beacon.cs
+++ Controlzmo/Systems/Lights/Beacon.cs
@@ -1,5 +1,4 @@
 ﻿using Controlzmo.Hubs;
-using Controlzmo.Systems.JetBridge;
 using Microsoft.Extensions.DependencyInjection;
 using SimConnectzmo;
 using System;
@@ -7,43 +6,20 @@ using System;
 namespace Controlzmo.Systems.Lights
 {
     [Component]
+    public class BeaconLightSetEvent : IEvent
+    {
+        public string SimEvent() => "BEACON_LIGHTS_SET";
+    }
+
+    [Component]
     public class BeaconLight : ISettable<bool?>
     {
-        private readonly JetBridgeSender sender;
+        private readonly BeaconLightSetEvent @event;
 
-        public BeaconLight(IServiceProvider sp) => sender = sp.GetRequiredService<JetBridgeSender>();
+        public BeaconLight(IServiceProvider sp) => this.@event = sp.GetRequiredService<BeaconLightSetEvent>();
 
         public string GetId() => "beaconLight";
 
-        public void SetInSim(ExtendedSimConnect simConnect, bool? value)
-        {
-            var desiredValue = value == true ? 1 : 0;
-//TODO: try using BEACON_LIGHTS_SET, with two parameters, the state 0/1 and the light index
-            sender.Execute(simConnect, $"(A:LIGHT BEACON, Bool) {desiredValue} != if{{ 0 (>K:TOGGLE_BEACON_LIGHTS) }}");
-        }
-    }
-
-    [Component]
-    public class ToggleBeaconLightsEvent : IEvent
-    {
-        public string SimEvent() => "TOGGLE_BEACON_LIGHTS";
-    }
-
-    [Component]
-    public class BeaconLightsSetter : ISettable<bool>
-    {
-        private readonly ToggleBeaconLightsEvent toggleBeaconLightsEvent;
-
-        public BeaconLightsSetter(ToggleBeaconLightsEvent toggleBeaconLightsEvent)
-        {
-            this.toggleBeaconLightsEvent = toggleBeaconLightsEvent;
-        }
-
-        public string GetId() => "lightsBeacon";
-
-        public void SetInSim(ExtendedSimConnect simConnect, bool value)
-        {
-            simConnect.SendEvent(toggleBeaconLightsEvent);
-        }
+        public void SetInSim(ExtendedSimConnect simConnect, bool? value) => simConnect.SendEvent(@event, value == true ? 1u : 0u);
     }
 }
