e9ca5cbfb6f81c9574f62cd4faee51fb7cba322c
Gizmo <gizmo@davegymer.org>
2023-09-21 19:46:27 +0100

    Completely untested experimental support for vJoy.

--- Controlzmo/Views/ResetView.cs
+++ Controlzmo/Views/ResetView.cs
@@ -4,7 +4,7 @@ using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
 using System;
 using System.Runtime.InteropServices;
-//using System.Windows.Forms; https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys?view=windowsdesktop-7.0 no right alt?
+using vJoy.Wrapper;
 
 namespace Controlzmo.Views
 {
@@ -29,40 +29,34 @@ namespace Controlzmo.Views
 // Some of these might be interesting: COCKPIT CAMERA HEADLOOK (1 free 2 head), COCKPIT CAMERA HEIGHT (50% is "normal"), COCKPIT CAMERA ZOOM (default 50%)
         [SimVar("CAMERA REQUEST ACTION", null, SIMCONNECT_DATATYPE.INT32, 0.5f)]
         public Int32 resetAction;
+        [SimVar("PLANE ALT ABOVE GROUND", "feet", SIMCONNECT_DATATYPE.INT32, 50f)]
+        public Int32 terrainHeight;
     }
-
-    // As far as we can tell, there's actually NO working support for sending the keypresses for these. :-( Perhaps we can actually send the keypresses...?
-    [Component]
-    // Relies on mapping INPUT.CAMERA_USER_LOAD_5? Legacy,also  doesn't work: VIEW_CAMERA_SELECT_5
-    // Note that this won't current work as we're using SimConnect_MapClientEventToSimEvent but we actually need a different sort, SimConnect_MapInputEventToClientEvent
-    // But that's probably not right anyway as I think you can only ask to have sim events sent for specific input events, not inject them...
-    public class QuickView5 : IEvent { public string SimEvent() => "PAN_RESET"; }
-// What event is the equivalent of "INPUT.CAMERA_USER_SAVE_5"?
-// What about VK_RMENU+VK_NUMPAD0 etc
-// We'll probably have to use vJoy and feed it instructions, mapping it's virtual buttons to the 10 quickviews.
-// Latest: https://github.com/njz3/vJoy; UDP feeder https://github.com/klach/vjoy-udp-feeder
 
     [Component]
     public class ResetView : DataListener<ResetViewData>
     {
         private readonly ILogger _logger;
-        private readonly QuickView5 setEvent;
+        private readonly VirtualJoystick vJoy;
 
         public ResetView(IServiceProvider sp)
         {
             _logger = sp.GetRequiredService<ILogger<ResetView>>();
-            setEvent = sp.GetRequiredService<QuickView5>();
+            vJoy = new VirtualJoystick(1);
+            vJoy.Aquire();
         }
 
         public override void Process(ExtendedSimConnect simConnect, ResetViewData data)
         {
-_logger.LogCritical($"reset view {data.cameraState}/{data.cameraSubState} type/index {data.cameraViewType}/{data.cameraViewIndex} zoom {data.cockpitCameraZoom}/{data.chaseCameraZoom}");
+_logger.LogCritical($"reset view {data.cameraState}/{data.cameraSubState} type/index {data.cameraViewType}/{data.cameraViewIndex} zoom {data.cockpitCameraZoom}/{data.chaseCameraZoom} above ground {data.terrainHeight}");
             if (data.cameraState == 2) // Cockpit
             {
-                data.cameraSubState = 1;
-                data.cameraViewType = 1;
-                data.cameraViewIndex = 1; // Doesn't seem to take effect. :-(
+                //data.cameraSubState = 1;
+                //data.cameraViewType = 1;
+                //data.cameraViewIndex = 1; // Doesn't seem to take effect. :-(
                 //data.cockpitCameraZoom = 50;
+                vJoy.SetJoystickButton(true, 1);
+                vJoy.SetJoystickButton(false, 1);
             }
             else if (data.cameraState == 3)
             {
