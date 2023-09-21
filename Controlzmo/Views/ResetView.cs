7c523cde8f67a4cf6e5a5ac559217b26ee514e72
Dave Gymer <gizmo@davegymer.org>
2023-09-21 16:37:35 +0100

    More view experiments.

--- Controlzmo/Views/ResetView.cs
+++ Controlzmo/Views/ResetView.cs
@@ -22,16 +22,25 @@ namespace Controlzmo.Views
         public Int32 cameraViewType;
         [SimVar("CAMERA VIEW TYPE AND INDEX:1", null, SIMCONNECT_DATATYPE.INT32, 0.5f)]
         public Int32 cameraViewIndex;
-// CHASE CAMERA ZOOM - set to 50% or send event?
+        [SimVar("CHASE CAMERA ZOOM", "percent", SIMCONNECT_DATATYPE.INT32, 0.5f)]
+        public Int32 chaseCameraZoom;
+        [SimVar("COCKPIT CAMERA ZOOM", "percent", SIMCONNECT_DATATYPE.INT32, 0.5f)]
+        public Int32 cockpitCameraZoom;
 // Some of these might be interesting: COCKPIT CAMERA HEADLOOK (1 free 2 head), COCKPIT CAMERA HEIGHT (50% is "normal"), COCKPIT CAMERA ZOOM (default 50%)
+        [SimVar("CAMERA REQUEST ACTION", null, SIMCONNECT_DATATYPE.INT32, 0.5f)]
+        public Int32 resetAction;
     }
 
     // As far as we can tell, there's actually NO working support for sending the keypresses for these. :-( Perhaps we can actually send the keypresses...?
     [Component]
-// Relies on mapping INPUT.CAMERA_USER_LOAD_5? Legacy,also  doesn't work: VIEW_CAMERA_SELECT_5
-    public class QuickView5 : IEvent { public string SimEvent() => "VK_RMENU+VK_NUMPAD0"; }
+    // Relies on mapping INPUT.CAMERA_USER_LOAD_5? Legacy,also  doesn't work: VIEW_CAMERA_SELECT_5
+    // Note that this won't current work as we're using SimConnect_MapClientEventToSimEvent but we actually need a different sort, SimConnect_MapInputEventToClientEvent
+    // But that's probably not right anyway as I think you can only ask to have sim events sent for specific input events, not inject them...
+    public class QuickView5 : IEvent { public string SimEvent() => "PAN_RESET"; }
 // What event is the equivalent of "INPUT.CAMERA_USER_SAVE_5"?
 // What about VK_RMENU+VK_NUMPAD0 etc
+// We'll probably have to use vJoy and feed it instructions, mapping it's virtual buttons to the 10 quickviews.
+// Latest: https://github.com/njz3/vJoy; UDP feeder https://github.com/klach/vjoy-udp-feeder
 
     [Component]
     public class ResetView : DataListener<ResetViewData>
@@ -47,15 +56,21 @@ namespace Controlzmo.Views
 
         public override void Process(ExtendedSimConnect simConnect, ResetViewData data)
         {
-_logger.LogCritical($"reset view {data.cameraState}/{data.cameraSubState} {data.cameraViewType}/{data.cameraViewIndex}");
+_logger.LogCritical($"reset view {data.cameraState}/{data.cameraSubState} type/index {data.cameraViewType}/{data.cameraViewIndex} zoom {data.cockpitCameraZoom}/{data.chaseCameraZoom}");
             if (data.cameraState == 2) // Cockpit
             {
-//TODO: can we at least reset the zoom to 0?
-                simConnect.SendEvent(setEvent);
+                data.cameraSubState = 1;
+                data.cameraViewType = 1;
+                data.cameraViewIndex = 1; // Doesn't seem to take effect. :-(
+                //data.cockpitCameraZoom = 50;
             }
-            else
-                return;
-            //simConnect.SendDataOnSimObject(data);
+            else if (data.cameraState == 3)
+            {
+                //data.chaseCameraZoom = 50;
+                //simConnect.SendEvent(setEvent, 0);
+            }
+            data.resetAction = 1;
+            simConnect.SendDataOnSimObject(data);
         }
     }
 }
