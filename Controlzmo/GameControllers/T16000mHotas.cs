447b579c787098b1337f8a4c882bccccfc834553
Dave Gymer <gizmo@davegymer.org>
2023-09-22 15:17:11 +0100

    Stuff I missed. :-(

--- Controlzmo/GameControllers/T16000mHotas.cs
+++ Controlzmo/GameControllers/T16000mHotas.cs
@@ -1,8 +1,5 @@
-﻿using Controlzmo.Systems.Spoilers;
-using Controlzmo.Views;
+﻿using Controlzmo.Views;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
 using System;
 using System.Collections.Generic;
@@ -12,14 +9,10 @@ namespace Controlzmo.GameControllers
     [Component]
     public class T16000mHotas : GameController
     {
-        private readonly MoreSpoiler moreListener;
-        private readonly LessSpoiler lessListener;
         private readonly IEnumerable<IButtonCallback> hotasViews;
 
         public T16000mHotas(IServiceProvider sp) : base(sp, 14, 8, 1)
         {
-            moreListener = sp.GetRequiredService<MoreSpoiler>();
-            lessListener = sp.GetRequiredService<LessSpoiler>();
             hotasViews = sp.GetServices<IButtonCallback>();
         }
 
@@ -32,42 +25,34 @@ namespace Controlzmo.GameControllers
         /* Switches
         0 top coolie hat (directions reported as if looking directly at it)
          */
-        private static readonly int AXIS_MINISTICK_HORIZONTAL = 0; // 0 left->1 right
-        private static readonly int AXIS_MINISTICK_VERTICAL = 1; // 0 up->1 down
-        private static readonly int AXIS_THROTTLE = 2; // 1 ide->0 max
-        private static readonly int AXIS_RUDDER_PADDLES = 5; // 0 left->1 right
-        private static readonly int AXIS_RUDDER_PEDDLES = 6; // 0 left->1 right
-        private static readonly int AXIS_WHEEL = 7; // ThrustMaster call it "Antenna"; 0 aft->1 back.
+        public static readonly int AXIS_MINISTICK_HORIZONTAL = 0; // 0 left->1 right
+        public static readonly int AXIS_MINISTICK_VERTICAL = 1; // 0 up->1 down
+        public static readonly int AXIS_THROTTLE = 2; // 1 ide->0 max
+        public static readonly int AXIS_RUDDER_PADDLES = 5; // 0 left->1 right
+        public static readonly int AXIS_RUDDER_PEDDLES = 6; // 0 left->1 right
+        public static readonly int AXIS_WHEEL = 7; // ThrustMaster call it "Antenna"; 0 aft->1 back.
 
-        /*1 front left red
-          2 front right red
-          3 front rocker up
-          4 front rocker down
-          6 middle big "rocker" pad up
-          7 middle big "rocker" pad fore
-          8 middle big "rocker" pad down
-          9 middle big "rocker" pad aft
-          10 bottom "castle" up
-          12 bottom "castle" down */
         public static readonly int BUTTON_SIDE_RED = 0;
+        public static readonly int BUTTON_FRONT_LEFT_RED = 1;
+        public static readonly int BUTTON_FRONT_RIGHT_RED = 2;
+        public static readonly int BUTTON_FRONT_ROCKER_UP = 3;
+        public static readonly int BUTTON_FRONT_ROCKER_DOWN = 4;
         public static readonly int BUTTON_MINISTICK = 5;
-        public static readonly int BUTTON_BOTTOM_HAT_FORE = 11; // bottom "castle" fore
-        public static readonly int BUTTON_BOTTOM_HAT_AFT = 13; // bottom "castle" aft
+        // 6 middle big "rocker" pad up
+        // 7 middle big "rocker" pad fore
+        // 8 middle big "rocker" pad down
+        // 9 middle big "rocker" pad aft
+        // Bottom hat or "castle".
+        public static readonly int BUTTON_BOTTOM_HAT_UP = 10;
+        public static readonly int BUTTON_BOTTOM_HAT_FORE = 11;
+        public static readonly int BUTTON_BOTTOM_HAT_DOWN = 12;
+        public static readonly int BUTTON_BOTTOM_HAT_AFT = 13;
         protected override void OnUpdate(ExtendedSimConnect simConnect)
         {
-            /*for (int i = 0; i < axesOld.Length; ++i)
-                if (axesOld[i] != axesNew[i])
-                    _log.LogDebug($"HOTAS: axes[{i}] {axesOld[i]} -> {axesNew[i]}");*/
-            if (!buttonsOld[BUTTON_BOTTOM_HAT_FORE] && buttonsNew[BUTTON_BOTTOM_HAT_FORE]) {
-                _log.LogDebug("User has asked for less speedbrake");
-                simConnect.RequestDataOnSimObject(lessListener, SIMCONNECT_CLIENT_DATA_PERIOD.ONCE);
-            }
-            if (!buttonsOld[BUTTON_BOTTOM_HAT_AFT] && buttonsNew[BUTTON_BOTTOM_HAT_AFT])
-            {
-                _log.LogDebug("User has asked for more speedbrake");
-                simConnect.RequestDataOnSimObject(moreListener, SIMCONNECT_CLIENT_DATA_PERIOD.ONCE);
-            }
 #if false
+            for (int i = 0; i < axesOld.Length; ++i)
+                if (axesOld[i] != axesNew[i])
+                    _log.LogDebug($"HOTAS: axes[{i}] {axesOld[i]} -> {axesNew[i]}");
             for (int i = 0; i < this.buttonsOld.Length; ++i)
                 if (buttonsOld[i] != buttonsNew[i])
                     _log.LogDebug($"Button {i} now {buttonsNew[i]}");
