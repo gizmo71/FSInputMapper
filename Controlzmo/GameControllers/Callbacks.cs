b7765e8fe3399d0f96d95745892542ab399d2b1f
Dave Gymer <gizmo@davegymer.org>
2023-09-23 07:10:43 +0100

    Rename and split source up and add unimplemented ISwitchCallback.

--- Controlzmo/GameControllers/Callbacks.cs
+++ Controlzmo/GameControllers/Callbacks.cs
@@ -8,4 +8,10 @@ namespace Controlzmo.GameControllers
         public virtual void OnPress(ExtendedSimConnect _) { }
         public virtual void OnRelease(ExtendedSimConnect _) { }
     }
+
+    public interface ISwitchCallback<C> where C : IGameController
+    {
+        public abstract int GetSwitch();
+        public abstract void OnChange(ExtendedSimConnect _);
+    }
 }
