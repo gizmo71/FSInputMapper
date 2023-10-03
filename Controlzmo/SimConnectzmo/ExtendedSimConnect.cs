4b0fd8c013c5046411b0b8380a4222153d4f7053
Dave Gymer <gizmo@davegymer.org>
2023-10-03 16:39:40 +0100

    Semi-working button mapping thingy.

--- Controlzmo/SimConnectzmo/ExtendedSimConnect.cs
+++ Controlzmo/SimConnectzmo/ExtendedSimConnect.cs
@@ -30,7 +30,7 @@ namespace SimConnectzmo
         private Dictionary<Type, STRUCT>? typeToStruct;
         private Dictionary<Type, string>? typeToClientDataName;
         private Dictionary<IDataListener, REQUEST>? typeToRequest;
-        private Dictionary<IEvent, EVENT>? eventToEnum;
+        internal Dictionary<IEvent, EVENT>? eventToEnum;
         private Dictionary<IEventNotification, EVENT>? notificationsToEvent;
         private IEnumerable<IOnSimConnection>? onConnectionHandlers;
         private IEnumerable<IOnSimStarted>? onSimStartedHandlers;
@@ -402,11 +402,11 @@ _logging!.LogDebug($"Received {e} for {String.Join(", ", notifications)}: {Conve
 
     [Component]
     [RequiredArgsConstructor]
-    public partial class Wibbleator : IButtonCallback<T16000mHotas>
+    public partial class Wibbleator : IButtonCallback<T16000mHotas>, IEvent, IEventNotification
     {
         private readonly ILogger<Wibbleator> log;
         public int GetButton() => T16000mHotas.BUTTON_FRONT_LEFT_RED;
-        
+
         public virtual void OnPress(ExtendedSimConnect sc) {
             //TODO: can we use this to find new events, if there are any? And perhaps avoid vJoy?
             sc.EnumerateControllers();
@@ -415,6 +415,7 @@ _logging!.LogDebug($"Received {e} for {String.Join(", ", notifications)}: {Conve
 
         public void OnRecvControllersList(ExtendedSimConnect sc, SIMCONNECT_RECV_CONTROLLERS_LIST data)
         {
+try {
             log.LogCritical($"wibbleate 1 @ {data.dwEntryNumber}! {data.rgData.Length} v {data.dwArraySize}");
             for (var i = 0 ; i < data.dwArraySize; ++i)
             {
@@ -424,7 +425,26 @@ _logging!.LogDebug($"Received {e} for {String.Join(", ", notifications)}: {Conve
                 // ProductID is the same as the USB one. No VendorID though, making it not far off useless!
                 // CompositeID always seems to be 0.
                 log.LogCritical($"wibbleate 1 [{i}] = {item.DeviceName}@{item.DeviceId} = {item.ProductId} and {item.CompositeID}");
+                if ("TWCS Throttle" == item.DeviceName)
+                {
+                    EVENT @event = sc.eventToEnum![this];
+                    var inputDefinition = $"joystick:{item.DeviceId}:button:{T16000mHotas.BUTTON_FRONT_LEFT_RED}";
+                    sc.RemoveInputEvent(GROUP.JUST_MASKABLE, inputDefinition);
+//TODO: how do we avoid having down AND up events? Can i use 0 in one of them?
+                    sc.MapInputEventToClientEvent_EX1(GROUP.JUST_MASKABLE, inputDefinition, @event, 0, @event, 0, true);
+                    log.LogCritical($"Mapped event => {sc.GetLastSentPacketID()}");
+                }
             }
+} catch (Exception e) { log.LogCritical(e, "Bugger"); }
         }
+
+        public IEvent GetEvent() => this;
+
+        public void OnRecieve(ExtendedSimConnect simConnect, SIMCONNECT_RECV_EVENT data)
+        {
+            log.LogCritical("Got the test event!");
+        }
+
+        public string SimEvent() => "gizmo.test1";
     }
 }
