5f8311ca73c9f61efbfde0471a5f5c683eb0c30b
Dave Gymer <gizmo@davegymer.org>
2023-11-12 10:49:43 +0000

    Use shared camera state for external toggle.

--- Controlzmo/Views/CockpitExternalToggle.cs
+++ Controlzmo/Views/CockpitExternalToggle.cs
@@ -1,45 +1,36 @@
 ﻿using Controlzmo.GameControllers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
 using System;
-using System.Runtime.InteropServices;
 
 namespace Controlzmo.Views
 {
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
-    public struct CockpitExternalToggleData
-    {
-        [SimVar("CAMERA STATE", "enum", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 cameraState;
-    }
-
     [Component]
-    public class CockpitExternalToggle : DataListener<CockpitExternalToggleData>, IButtonCallback<T16000mHotas>
+    public class CockpitExternalToggle : IButtonCallback<T16000mHotas>
     {
         private const Int32 COCKPIT = 2;
         private const Int32 CHASE = 3;
 
         protected readonly ILogger _logger;
         private readonly VirtualJoy vJoy;
+        private readonly CameraState state;
 
         public CockpitExternalToggle(IServiceProvider sp)
         {
             _logger = sp.GetRequiredService<ILogger<CockpitExternalToggle>>();
+            state = sp.GetRequiredService<CameraState>();
             vJoy = sp.GetRequiredService<VirtualJoy>();
         }
 
         public int GetButton() => T16000mHotas.BUTTON_SIDE_RED;
-        public void OnPress(ExtendedSimConnect simConnect) => simConnect.RequestDataOnSimObject(this, SIMCONNECT_CLIENT_DATA_PERIOD.ONCE);
-
-        public override void Process(ExtendedSimConnect simConnect, CockpitExternalToggleData data)
+        public void OnPress(ExtendedSimConnect simConnect)
         {
-            if (data.cameraState == COCKPIT || data.cameraState == CHASE) {
-_logger.LogWarning($"Sedning 114 for {data.cameraState}");
+            if (state.Current.cameraState == COCKPIT || state.Current.cameraState == CHASE) {
+_logger.LogWarning($"Sedning 114 for {state.Current.cameraState}");
                 vJoy.getController().QuickClick(114u);
             } else
-                _logger.LogWarning($"Wrong camera state for chase view toggle {data.cameraState}");
+                _logger.LogWarning($"Wrong camera state for chase view toggle {state.Current.cameraState}");
         }
     }
 }
