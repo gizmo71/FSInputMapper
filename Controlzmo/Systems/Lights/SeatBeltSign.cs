75c0f3fe14815e78518dbd31dc08cd213282c75c
Dave Gymer <gizmo@davegymer.org>
2024-04-18 17:08:11 +0100

    Fenix seat belts.

--- Controlzmo/Systems/Lights/SeatBeltSign.cs
+++ Controlzmo/Systems/Lights/SeatBeltSign.cs
@@ -1,25 +1,23 @@
 ﻿using Controlzmo.Hubs;
 using Controlzmo.Systems.JetBridge;
-using Microsoft.Extensions.DependencyInjection;
+using Lombok.NET;
 using SimConnectzmo;
-using System;
 
 namespace Controlzmo.Systems.Lights
 {
     [Component]
-    public class SeatBeltSign : ISettable<bool?>
+    [RequiredArgsConstructor]
+    public partial class SeatBeltSign : ISettable<bool?>
     {
         private readonly JetBridgeSender sender;
-
-        public SeatBeltSign(IServiceProvider sp) => sender = sp.GetRequiredService<JetBridgeSender>();
 
         public string GetId() => "seatBeltSign";
 
         public void SetInSim(ExtendedSimConnect simConnect, bool? value)
         {
             var desiredValue = value == true ? 1 : 0;
-            sender.Execute(simConnect, $"(A:CABIN SEATBELTS ALERT SWITCH,Bool) {desiredValue} !="
-                + " if{ (>K:CABIN_SEATBELTS_ALERT_SWITCH_TOGGLE) }");
+            sender.Execute(simConnect, simConnect.IsFenix ? $"{desiredValue} (>L:S_OH_SIGNS)" :
+                "(A:CABIN SEATBELTS ALERT SWITCH,Bool) " + desiredValue + " != if{ (>K:CABIN_SEATBELTS_ALERT_SWITCH_TOGGLE) }");
         }
     }
 }
