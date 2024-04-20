bb4edff99540920b97648f81717a6bd935d511f5
Dave Gymer <gizmo@davegymer.org>
2024-04-20 08:34:56 +0100

    Baro fully working with Fenix.

--- Controlzmo/Systems/EfisControlPanel/Baro.cs
+++ Controlzmo/Systems/EfisControlPanel/Baro.cs
@@ -23,21 +23,26 @@ namespace Controlzmo.Systems.EfisControlPanel
         {
             string command;
             if (value == "pull")
-                command = @"(L:XMLVAR_Baro1_Mode) 2 | (>L:XMLVAR_Baro1_Mode)";
+                command = simConnect.IsFenix ? "1 (>L:S_FCU_EFIS1_BARO_STD)" : @"(L:XMLVAR_Baro1_Mode) 2 | (>L:XMLVAR_Baro1_Mode)";
             else if (value == "push")
-                command = @"(L:XMLVAR_Baro1_Mode) 2 & 0 != if{ 2 } els{ 1 } (L:XMLVAR_Baro1_Mode) ^ (>L:XMLVAR_Baro1_Mode)";
+                command = simConnect.IsFenix ? "0 (>L:S_FCU_EFIS1_BARO_STD)" :
+                    @"(L:XMLVAR_Baro1_Mode) 2 & 0 != if{ 2 } els{ 1 } (L:XMLVAR_Baro1_Mode) ^ (>L:XMLVAR_Baro1_Mode)";
+            else if (simConnect.IsFenix && value!.EndsWith("Dec"))
+                command = "(L:E_FCU_EFIS1_BARO) -- (>L:E_FCU_EFIS1_BARO)";
             else if (value == "hPaDec")
                 command = @"(L:XMLVAR_Baro1_Mode) 2 & 0 == if{ 1 (A:KOHLSMAN SETTING MB:1, mbars) near -- 16 * (>K:2:KOHLSMAN_SET) }";
             else if (value == "inHgDec")
                 command = @"(L:XMLVAR_Baro1_Mode) 2 & 0 == if{ 1 (A:KOHLSMAN SETTING MB:1, mbars) 0.3386389 / near -- 5.4182224 * (>K:2:KOHLSMAN_SET) } }";
+            else if (simConnect.IsFenix && value!.EndsWith("Inc"))
+                command = "(L:E_FCU_EFIS1_BARO) ++ (>L:E_FCU_EFIS1_BARO)";
             else if (value == "hPaInc")
                 command = @"(L:XMLVAR_Baro1_Mode) 2 & 0 == if{ 1 (A:KOHLSMAN SETTING MB:1, mbars) near ++ 16 * (>K:2:KOHLSMAN_SET) }";
             else if (value == "inHgInc")
                 command = @"(L:XMLVAR_Baro1_Mode) 2 & 0 == if{ 1 (A:KOHLSMAN SETTING MB:1, mbars) 0.3386389 / near ++ 5.4182224 * (>K:2:KOHLSMAN_SET) } }";
             else if (value == "inHg")
-                command = "0 (>L:XMLVar_Baro_Selector_HPA_1)";
+                command = simConnect.IsFenix ? "0 (>L:S_FCU_EFIS1_BARO_MODE)" : "0 (>L:XMLVar_Baro_Selector_HPA_1)";
             else if (value == "hPa")
-                command = "1 (>L:XMLVar_Baro_Selector_HPA_1)";
+                command = simConnect.IsFenix ? "1 (>L:S_FCU_EFIS1_BARO_MODE)" : "1 (>L:XMLVar_Baro_Selector_HPA_1)";
             else
                 return;
 
@@ -54,10 +59,14 @@ namespace Controlzmo.Systems.EfisControlPanel
         public float kohlsmanMB;
         [SimVar("KOHLSMAN SETTING HG", "inHg", SIMCONNECT_DATATYPE.FLOAT32, 0.01f)]
         public float kohlsmanHg;
-        [SimVar("L:XMLVar_Baro_Selector_HPA_1", "bool", SIMCONNECT_DATATYPE.INT32, 0.4f)]
-        public Int32 baro1Units; // InHg if 0, otherwise hPa
         [SimVar("L:XMLVAR_Baro1_Mode", "", SIMCONNECT_DATATYPE.INT32, 0.4f)]
         public Int32 baro1Mode; // Bit 0: 1 QNH, 0 QFE; Bit 1: 2 standard, otherwise as bit 1
+        [SimVar("L:I_FCU_EFIS1_QNH", "", SIMCONNECT_DATATYPE.INT32, 0.4f)]
+        public Int32 baro1ModeFenix; // 0 Std, 1 QNH
+        [SimVar("L:XMLVar_Baro_Selector_HPA_1", "bool", SIMCONNECT_DATATYPE.INT32, 0.4f)]
+        public Int32 baro1Units; // InHg if 0, otherwise hPa
+        [SimVar("L:S_FCU_EFIS1_BARO_MODE", "", SIMCONNECT_DATATYPE.INT32, 0.4f)]
+        public Int32 baro1UnitsFenix; // 0 InHg, 1 hPa
     }
 
     [Component]
@@ -72,10 +81,10 @@ namespace Controlzmo.Systems.EfisControlPanel
         public override void Process(ExtendedSimConnect simConnect, BaroData data)
         {
             string composite = "SStd ";
-            if ((data.baro1Mode & 2) == 0)
+            if (simConnect.IsFenix ? data.baro1ModeFenix == 1 : (data.baro1Mode & 2) == 0)
             {
-                var value = data.baro1Units == 0 ? data.kohlsmanHg * 100 : data.kohlsmanMB;
-                composite = ((data.baro1Mode & 1) == 1 ? "N" : "F") + $"{value,4:0}";
+                var value = (simConnect.IsFenix ? data.baro1UnitsFenix : data.baro1Units) == 0 ? data.kohlsmanHg * 100 : data.kohlsmanMB;
+                composite = (!simConnect.IsFBW || (data.baro1Mode & 1) == 1 ? "N" : "F") + $"{value,4:0}";
             }
             serial.SendLine($"baro={composite}");
         }
