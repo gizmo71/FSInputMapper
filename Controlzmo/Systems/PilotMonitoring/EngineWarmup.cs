35acc2219fc1569029541d2d628d1e53991db861
Dave Gymer <gizmo@davegymer.org>
2024-05-06 08:49:17 +0100

    Attempt to push Cabin All call button when warmup timer expires in Fenix. Untested.

--- Controlzmo/Systems/PilotMonitoring/EngineWarmup.cs
+++ Controlzmo/Systems/PilotMonitoring/EngineWarmup.cs
@@ -2,7 +2,6 @@
 using Controlzmo.Systems.EfisControlPanel;
 using Controlzmo.Systems.JetBridge;
 using Lombok.NET;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
 using System;
@@ -45,7 +44,7 @@ namespace Controlzmo.Systems.PilotMonitoring
 
         public override void Process(ExtendedSimConnect simConnect, EngineWarmupData data)
         {
-            //TODO: support different numbers of engines... MSFS now supports up to 16...
+//TODO: support different numbers of engines... MSFS now supports up to 16...
             int engines = 2;
             int running = data.combustion1 + data.combustion2;
             if (running == engines - 1 && !isArmed)
@@ -68,7 +67,11 @@ namespace Controlzmo.Systems.PilotMonitoring
             {
                 chronoButton.SetInSim(simConnect, null);
                 chronoButton.SetInSim(simConnect, null);
-                jetbridge.Execute(simConnect, "1 (>L:A32NX_CABIN_READY)");
+                if (simConnect.IsFBW)
+                    jetbridge.Execute(simConnect, "1 (>L:A32NX_CABIN_READY)");
+                else if (simConnect.IsFenix)
+                    for (var i = 0; i < 2; ++i)
+                        jetbridge.Execute(simConnect, "(L:S_OH_CALLS_ALL) ++ (>L:S_OH_CALLS_ALL)");
                 isArmed = false;
                 warmAt = null;
             }
