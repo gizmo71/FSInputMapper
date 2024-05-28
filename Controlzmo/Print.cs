35158c5d5034db5a6ba6232331adff70e8dedcee
Gizmo <gizmo@davegymer.org>
2024-05-28 21:01:24 +0100

    WIP: METAR fetch and print.

---
+++ Controlzmo/Print.cs
@@ -0,0 +1,21 @@
+﻿using Controlzmo.Hubs;
+using Lombok.NET;
+using Microsoft.Extensions.Logging;
+using SimConnectzmo;
+
+namespace Controlzmo
+{
+    [Component]
+    [RequiredArgsConstructor]
+    public partial class Print : ISettable<string>
+    {
+        private readonly ILogger<Print> _log;
+
+        public string GetId() => "printText";
+
+        public void SetInSim(ExtendedSimConnect simConnect, string? value)
+        {
+            _log.LogCritical("TODO: print {}", value);
+        }
+    }
+}
---
+++ Controlzmo/Systems/Atc/Metar.cs
@@ -0,0 +1,24 @@
+﻿using Controlzmo.Hubs;
+using Lombok.NET;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using SimConnectzmo;
+
+namespace Controlzmo.Systems.Atc
+{
+    [Component]
+    [RequiredArgsConstructor]
+    public partial class FetchMetar : ISettable<string>
+    {
+        private readonly IHubContext<ControlzmoHub, IControlzmoHub> hub;
+        private readonly ILogger<FetchMetar> _log;
+
+        public string GetId() => "fetchMetar";
+
+        public void SetInSim(ExtendedSimConnect simConnect, string? value)
+        {
+            _log.LogCritical("Fetch METAR for {}", value);
+            hub.Clients.All.SetFromSim("metar", $"TODO: METAR for {value}");
+        }
+    }
+}
