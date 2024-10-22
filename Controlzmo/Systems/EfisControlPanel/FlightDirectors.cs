0cfc4189275df6b035fde1ed805946eea5ef6a6e
Dave Gymer <gizmo@davegymer.org>
2024-10-22 16:35:51 +0100

    Add button for toggling the flight director.

--- Controlzmo/Systems/EfisControlPanel/FlightDirectors.cs
+++ Controlzmo/Systems/EfisControlPanel/FlightDirectors.cs
@@ -1,39 +1,25 @@
-using Controlzmo.Serial;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.FlightSimulator.SimConnect;
+using Controlzmo.GameControllers;
+using Controlzmo.Systems.JetBridge;
+using Lombok.NET;
 using SimConnectzmo;
-using System;
-using System.Runtime.InteropServices;
 
 namespace Controlzmo.Systems.EfisControlPanel
 {
-    //TODO 1 or 2 (>K:TOGGLE_FLIGHT_DIRECTOR)
-
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
-    public struct FlightDirectorData
-    {
-        [SimVar("AUTOPILOT FLIGHT DIRECTOR ACTIVE:1", "Bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public int fdActive1;
-        [SimVar("AUTOPILOT FLIGHT DIRECTOR ACTIVE:2", "Bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public int fdActive2;
-    };
-
     [Component]
-    public class FlightDirectorListener : DataListener<FlightDirectorData>, IRequestDataOnOpen
+    [RequiredArgsConstructor]
+    public partial class FlightDirectorToggle : IButtonCallback<UrsaMinorFighterR>
     {
-        private readonly ILogger logging;
-        private readonly SerialPico serial;
+        private readonly JetBridgeSender sender;
 
-        public FlightDirectorListener(IServiceProvider sp)
+        public int GetButton() => UrsaMinorFighterR.BUTTON_LARGER_ROUND;
+
+        public virtual void OnPress(ExtendedSimConnect sc)
         {
-            logging = sp.GetRequiredService<ILogger<FlightDirectorListener>>();
-            serial = sp.GetRequiredService<SerialPico>();
+            if (sc.IsFenix)
+                for (int i = 2; i-- > 0; )
+                    sender.Execute(sc, $"{i} (>L:S_FCU_EFIS1_FD)");
+            else
+                sender.Execute(sc, "1 (>K:TOGGLE_FLIGHT_DIRECTOR)");
         }
-
-        public SIMCONNECT_PERIOD GetInitialRequestPeriod() => SIMCONNECT_PERIOD.SIM_FRAME;
-
-        public override void Process(ExtendedSimConnect simConnect, FlightDirectorData data)
-            => logging.LogError($"FdActive1={data.fdActive1} FdActive2={data.fdActive2}");
     }
 }
