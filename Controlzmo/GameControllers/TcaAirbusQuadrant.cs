9f2487eb61a75e6520f865a22c46eb7cc1461d90
Dave Gymer <gizmo@davegymer.org>
2024-11-21 13:00:19 +0000

    Add full set of input codes to both Ferrari gamepad and TCA throttles.

--- Controlzmo/GameControllers/TcaAirbusQuadrant.cs
+++ Controlzmo/GameControllers/TcaAirbusQuadrant.cs
@@ -1,8 +1,4 @@
-﻿using Controlzmo.Systems.Autothrust;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.FlightSimulator.SimConnect;
-using SimConnectzmo;
+﻿using SimConnectzmo;
 using System;
 
 namespace Controlzmo.GameControllers
@@ -10,12 +6,7 @@ namespace Controlzmo.GameControllers
     [Component]
     public class TcaAirbusQuadrant : GameController<TcaAirbusQuadrant>
     {
-        private readonly AutothrottleArmedDataListener autothrustListener;
-
-        public TcaAirbusQuadrant(IServiceProvider sp) : base(sp, 31, 7, 0)
-        {
-            autothrustListener = sp.GetRequiredService<AutothrottleArmedDataListener>();
-        }
+        public TcaAirbusQuadrant(IServiceProvider sp) : base(sp, 31, 7, 0) { }
 
         public override ushort Vendor() => 1103;
         public override ushort Product() => 1031;
@@ -24,21 +15,15 @@ namespace Controlzmo.GameControllers
         public static readonly int BUTTON_RIGHT_INTUITIVE_DISCONNECT = 1;
         public static readonly int BUTTON_LEFT_ENGINE_MASTER = 2;
         public static readonly int BUTTON_RIGHT_ENGINE_MASTER = 3;
-        /* Buttons
-        4 left fire "light"
-        4 right fire "light"
-        6 engine mode crank
-        7 engine mode ignition/start
-           Switches - are there any? */
+        public static readonly int BUTTON_LEFT_FIRE_LIGHT = 4;
+        public static readonly int BUTTON_RIGHT_FIRE_LIGHT = 5;
+        public static readonly int BUTTON_MODE_CRANK = 6;
+        public static readonly int BUTTON_MODE_START = 7;
         public static readonly int AXIS_LEFT_THRUST = 0;
         public static readonly int AXIS_RIGHT_THRUST = 1;
+
         protected override void OnUpdate(ExtendedSimConnect simConnect)
         {
-            if (buttonsNew[BUTTON_LEFT_INTUITIVE_DISCONNECT] && !buttonsOld[BUTTON_LEFT_INTUITIVE_DISCONNECT])
-            {
-                _log.LogDebug("User has asked to arm autothrust");
-                simConnect.RequestDataOnSimObject(autothrustListener, SIMCONNECT_CLIENT_DATA_PERIOD.ONCE);
-            }
         }
     }
 }
--- Controlzmo/GameControllers/TmFerrariGamepad.cs
+++ Controlzmo/GameControllers/TmFerrariGamepad.cs
@@ -11,29 +11,33 @@ namespace Controlzmo.GameControllers
         public override ushort Vendor() => 1103;
         public override ushort Product() => 45845;
 
-        /* Buttons
-        0 top right aft
-        1 top right left
-        2 top right right
-        3 top right fore
-        4 front top left
-        5 front bottom left
-        6 front top right
-        7 front top right
-        8 left underneath
-        9 right underneath
-        10 left stick
-        11 right stick
-        /* Axes
-        0 left stick roll (0 left->1 right)
-        0/1 d-pad roll/pitch (only with bottom light off)
-        1 left stick pitch (0 fore->1 aft)
-        2 right stick roll (0 left->1 right)
-        3 right stick pitch (0 fore->1 aft)
-        /* Switches
-        0 d-pad roll (only with bottom light on; 0 left->1 right)
-        1 d-pad pitch (only with bottom light on; 0 fore->1 aft)
-         */
+        public static readonly int BUTTON_TOP_RIGHT_AFT = 0;
+        public static readonly int BUTTON_TOP_RIGHT_LEFT = 1;
+        public static readonly int BUTTON_TOP_RIGHT_RIGHT = 2;
+        public static readonly int BUTTON_TOP_RIGHT_FORE = 3;
+        public static readonly int BUTTON_FRONT_TOP_LEFT = 4;
+        public static readonly int BUTTON_FRONT_BOTTOM_LEFT = 5;
+        public static readonly int BUTTON_FRONT_TOP_RIGHT = 6;
+        public static readonly int BUTTON_FRONT_BOTTOM_RIGHT = 7;
+        public static readonly int BUTTON_LEFT_UNDERNEATH = 8;
+        public static readonly int BUTTON_RIGHT_UNDERNEATH = 9;
+        public static readonly int BUTTON_LEFT_STICK = 10;
+        public static readonly int BUTTON_RIGHT_STICK = 11;
+
+        /// <summary>0 left->1 right (with bottom light off)</summary>
+        public static readonly int AXIS_LEFT_STICK_ROLL = 0;
+        /// <summary>0 fore->1 aft (with bottom light off)</summary>
+        public static readonly int AXIS_LEFT_STICK_PITCH = 1;
+        /// <summary>0 left->1 right</summary>
+        public static readonly int AXIS_RIGHT_STICK_ROLL = 2;
+        /// <summary>0 fore->1 aft</summary>
+        public static readonly int AXIS_RIGHT_STICK_PITCH = 3;
+
+        /// <summary>0 left->1 right (with bottom light on)</summary>
+        public static readonly int SWITCH_DPAD_ROLL = 0;
+        /// <summary>0 fore->1 aft (with bottom light on)</summary>
+        public static readonly int SWITCH_DPAD_PITCH = 1;
+
         protected override void OnUpdate(ExtendedSimConnect simConnect) {
         }
     }
