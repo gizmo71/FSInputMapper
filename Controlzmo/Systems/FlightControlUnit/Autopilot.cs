bde2b3816574156e4b3a63c1778194f4851ff1da
Dave Gymer <gizmo@davegymer.org>
2024-11-21 14:07:28 +0000

    Autopilot on/off.

---
+++ Controlzmo/Systems/FlightControlUnit/Autopilot.cs
@@ -0,0 +1,27 @@
+﻿using Controlzmo.GameControllers;
+using Lombok.NET;
+using SimConnectzmo;
+
+namespace Controlzmo.Systems.FlightControlUnit
+{
+    [Component] public class AutopilotOnEvent : IEvent { public string SimEvent() => "AUTOPILOT_ON"; }
+    [Component] public class AutopilotOffEvent : IEvent { public string SimEvent() => "AUTOPILOT_OFF"; }
+
+    [Component, RequiredArgsConstructor]
+    public partial class AutopilotOn : IButtonCallback<UrsaMinorFighterR>
+    {
+        private readonly AutopilotOnEvent _event;
+        public int GetButton() => UrsaMinorFighterR.BUTTON_SMALLER_ROUND;
+        public void OnPress(ExtendedSimConnect sc) => sc.SendEvent(_event);
+    }
+
+    [Component] public class LessFlapEvent : IEvent { public string SimEvent() => "FLAPS_DECR"; }
+
+    [Component, RequiredArgsConstructor]
+    public partial class AutopilotOff : IButtonCallback<UrsaMinorFighterR>
+    {
+        private readonly AutopilotOffEvent _event;
+        public int GetButton() => UrsaMinorFighterR.BUTTON_RED;
+        public void OnPress(ExtendedSimConnect sc) => sc.SendEvent(_event);
+    }
+}
