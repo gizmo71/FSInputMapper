fa55d329a63e1199e2ab7484c9259b4a9fd9d533
Dave Gymer <gizmo@davegymer.org>
2024-11-21 13:22:15 +0000

    Take control of parking break.

---
+++ Controlzmo/Systems/Controls/ParkingBrake.cs
@@ -0,0 +1,28 @@
+﻿using Controlzmo.GameControllers;
+using Lombok.NET;
+using SimConnectzmo;
+
+namespace Controlzmo.Systems.Controls
+{
+    [Component] public class SetParkingBrakeEvent : IEvent { public string SimEvent() => "PARKING_BRAKE_SET"; }
+
+    [Component, RequiredArgsConstructor]
+    public partial class ParkingBrake : IAxisCallback<T16000mHotas>
+    {
+        private readonly SetParkingBrakeEvent _event;
+
+        public int GetAxis() => T16000mHotas.AXIS_THROTTLE;
+
+        public void OnChange(ExtendedSimConnect sc, double old, double @new)
+        {
+            uint value;
+            if (@new > 0.75 && old <= 0.75)
+                value = 0;
+            else if (@new < 0.25 && old >= 0.25)
+                value = 1;
+            else
+                return;
+            sc.SendEvent(_event, value);
+        }
+    }
+}
