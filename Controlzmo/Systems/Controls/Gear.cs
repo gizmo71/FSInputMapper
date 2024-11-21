47110bdfaa1e486b7f743bf1ef6288d7d44df3c9
Dave Gymer <gizmo@davegymer.org>
2024-11-21 15:31:44 +0000

    Move gear handle in.

---
+++ Controlzmo/Systems/Controls/Gear.cs
@@ -0,0 +1,28 @@
+﻿using Controlzmo.GameControllers;
+using Lombok.NET;
+using SimConnectzmo;
+
+namespace Controlzmo.Systems.Controls
+{
+    [Component] public class SetGearEvent : IEvent { public string SimEvent() => "GEAR_SET"; }
+
+    [Component, RequiredArgsConstructor]
+    public partial class LandingGearHandle : IAxisCallback<UrsaMinorFighterR>
+    {
+        private readonly SetGearEvent _event;
+
+        public int GetAxis() => UrsaMinorFighterR.AXIS_THROTTLE;
+
+        public void OnChange(ExtendedSimConnect sc, double old, double @new)
+        {
+            uint value;
+            if (@new < 0.25 && old >= 0.25)
+                value = 0; // Up
+            else if (@new > 0.75 && old <= 0.75)
+                value = 1; // Down
+            else
+                return;
+            sc.SendEvent(_event, value);
+        }
+    }
+}
