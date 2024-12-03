e44031b26603be969a5946e945ddf68eec6be50f
Dave Gymer <gizmo@davegymer.org>
2024-12-03 16:59:42 +0000

    TO Config for A330.

--- Controlzmo/Systems/EcamControlPanel.cs
+++ Controlzmo/Systems/EcamControlPanel.cs
@@ -1,27 +1,26 @@
 ﻿using Controlzmo.Hubs;
+using Controlzmo.SimConnectzmo;
 using Controlzmo.Systems.JetBridge;
-using Microsoft.Extensions.DependencyInjection;
+using Lombok.NET;
 using SimConnectzmo;
 using System;
 using System.ComponentModel;
 
 namespace Controlzmo.Systems
 {
-    [Component]
-    public class EcamButtonTakeOffConfig : ISettable<string?>
+    [Component, RequiredArgsConstructor]
+    public partial class EcamButtonTakeOffConfig : ISettable<string?>
     {
         private readonly JetBridgeSender jetbridge;
-
-        public EcamButtonTakeOffConfig(IServiceProvider serviceProvider)
-        {
-            jetbridge = serviceProvider.GetRequiredService<JetBridgeSender>();
-        }
+        private readonly InputEvents inputEvents;
 
         public string GetId() => "ecamButtonTakeOffConfig";
 
         public void SetInSim(ExtendedSimConnect simConnect, string? value)
         {
-            String lvar = simConnect.IsFenix ? "S_ECAM_TO" : "A32NX_BTN_TOCONFIG";
+            String lvar = "A32NX_BTN_TOCONFIG";
+            if (simConnect.IsFenix) lvar = "S_ECAM_TO";
+            else if (simConnect.IsIniBuilds) lvar = "PUSH_ECAM_TOCONFIG";
             for (int i = 1; i >= 0; i--)
                 jetbridge.Execute(simConnect, $"{i} (>L:{lvar})");
         }
