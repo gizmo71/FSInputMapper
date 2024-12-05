b9021114be1ec42e3b08608e8d565891367bbba1
Dave Gymer <gizmo@davegymer.org>
2024-12-05 17:47:00 +0000

    Nasty hack to get to the A330 MCDU.

---
+++ Controlzmo/Views/ViewMcdu.cs
@@ -0,0 +1,24 @@
+﻿using Controlzmo.GameControllers;
+using Lombok.NET;
+using SimConnectzmo;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Controlzmo.Views
+{
+    [Component, RequiredArgsConstructor]
+    public partial class ViewMcdu : IButtonCallback<T16000mHotas>
+    {
+        private readonly VirtualJoy vJoy;
+
+        public int GetButton() => T16000mHotas.BUTTON_FRONT_LEFT_RED;
+
+        public virtual void OnPress(ExtendedSimConnect simConnect) {
+            Task task = vJoy.getController().QuickClick(90u);
+            for (int i = 0; i < 3; ++i)
+                task  = task.
+                    ContinueWith(_ => Thread.Sleep(100)).
+                    ContinueWith(_ => vJoy.getController().QuickClick(89u).Wait(300));
+        }
+    }
+}
