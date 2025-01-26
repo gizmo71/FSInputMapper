9a46425e1dc678f29eef6bad8f9c6250a41429f3
Dave Gymer <gizmo@davegymer.org>
2025-01-26 10:52:25 +0000

    Add A330 catch-all.

--- Controlzmo/SimConnectzmo/ExtendedSimConnect.cs
+++ Controlzmo/SimConnectzmo/ExtendedSimConnect.cs
@@ -397,9 +397,13 @@ _logging!.LogDebug($"Received {e} for {String.Join(", ", notifications)}: {Conve
         public string AircraftFile { get => aircraftFile; }
         /// <summary>Actually anything not specifically excluded (Fenix, Ini, B748, B38M).</summary>
         public bool IsFBW { get => !IsFenix && !IsIniBuilds && !IsB748 && !IsAsoboB38M; }
+        /// <summary>Any A330 or derivative</summary>
+        public bool IsA330 { get=> IsIni330 || IsIni337 | IsA339; }
         /// <summary>The experimental one (A380X-compatible).</summary>
         public bool IsA32NX { get => aircraftFile.StartsWith("FLYBYWIRE_A320_NEO"); }
         public bool IsA380X { get => aircraftFile.StartsWith("FLYBYWIRE_A380"); }
+        /// <summary>Headwind A339, post new cockpit.</summary>
+        public bool IsA339 { get => aircraftFile.StartsWith("HEADWIND_A330NEO"); }
         public bool IsFenix { get => aircraftFile.StartsWith("FNX_3"); }
         public bool IsIni320 { get => aircraftFile.Equals("MICROSOFT-AIRCRAFT-A320NEO") || aircraftFile.Equals("MICROSOFT-A320NEO\\PRESETS\\INIBUILDS\\A20N\\CONFIG"); }
         public bool IsIni321 { get => aircraftFile.Equals("MICROSOFT-A321\\PRESETS\\INIBUILDS\\A21N\\CONFIG"); }
