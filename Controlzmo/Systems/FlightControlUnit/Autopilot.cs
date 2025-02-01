a32ca0fac36a6448f37747c9d3af124c1db4233f
Dave Gymer <gizmo@davegymer.org>
2025-02-01 09:58:38 +0000

    Fix A32NX autopilot on.

--- Controlzmo/Systems/FlightControlUnit/Autopilot.cs
+++ Controlzmo/Systems/FlightControlUnit/Autopilot.cs
@@ -1,9 +1,11 @@
 ﻿using Controlzmo.GameControllers;
+using Controlzmo.Systems.JetBridge;
 using Lombok.NET;
 using SimConnectzmo;
 
 namespace Controlzmo.Systems.FlightControlUnit
 {
+//TODO: "on" doesn't work for the nwe A32NX. What about the A380X?
     [Component] public class AutopilotOnEvent : IEvent { public string SimEvent() => "AUTOPILOT_ON"; }
     [Component] public class AutopilotOffEvent : IEvent { public string SimEvent() => "AUTOPILOT_OFF"; }
 
@@ -11,8 +13,15 @@ namespace Controlzmo.Systems.FlightControlUnit
     public partial class AutopilotOn : IButtonCallback<UrsaMinorFighterR>
     {
         private readonly AutopilotOnEvent _event;
+        private readonly JetBridgeSender sender;
         public int GetButton() => UrsaMinorFighterR.BUTTON_SMALLER_ROUND;
-        public void OnPress(ExtendedSimConnect sc) => sc.SendEvent(_event);
+
+        public void OnPress(ExtendedSimConnect sc) {
+            if (sc.IsA32NX)
+                sender.Execute(sc, "(L:A32NX_AUTOPILOT_1_ACTIVE) if{ (>K:A32NX.FCU_AP_2_PUSH) } els{ (>K:A32NX.FCU_AP_1_PUSH) }");
+            else
+                sc.SendEvent(_event);
+        }
     }
 
     [Component] public class LessFlapEvent : IEvent { public string SimEvent() => "FLAPS_DECR"; }
