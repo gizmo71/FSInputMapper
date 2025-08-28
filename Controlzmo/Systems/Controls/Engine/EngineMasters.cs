b86a4f5622323d3be978991b36277fbd85dc8d2d
Dave Gymer <gizmo@davegymer.org>
2025-08-28 20:20:58 +0100

    Delay subsequent engine master changes by 3s for aircraft with more than two engines.

--- Controlzmo/Systems/Controls/Engine/EngineMasters.cs
+++ Controlzmo/Systems/Controls/Engine/EngineMasters.cs
@@ -1,64 +1,15 @@
 ﻿using Controlzmo.GameControllers;
-using Controlzmo.Hubs;
 using Controlzmo.Systems.JetBridge;
 using Lombok.NET;
-using Microsoft.AspNetCore.SignalR;
-using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
 using System;
-using System.Runtime.InteropServices;
+using System.Timers;
 
 namespace Controlzmo.Systems.Controls.Engine
 {
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
-    public struct EngineControlSelectData
-    {
-        [SimVar("ENGINE CONTROL SELECT", "Number", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 bitFlags; // LSB engine 1 etc
-        [SimVar("FUELSYSTEM VALVE SWITCH:1", "Bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 fsvs1;
-        [SimVar("FUELSYSTEM VALVE SWITCH:2", "Bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 fsvs2;
-        [SimVar("FUELSYSTEM VALVE SWITCH:3", "Bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 fsvs3;
-        [SimVar("FUELSYSTEM VALVE SWITCH:4", "Bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
-        public Int32 fsvs4;
-    };
-
-    [Component, RequiredArgsConstructor]
-    public partial class EngineControlSelectListener : DataListener<EngineControlSelectData>, IButtonCallback<UrsaMinorFighterR>, IRequestDataOnOpen
-    {
-        private readonly IHubContext<ControlzmoHub, IControlzmoHub> hub;
-
-        public int GetButton() => UrsaMinorFighterR.BUTTON_FAR_TRIGGER_PUSH;
-
-        public SIMCONNECT_PERIOD GetInitialRequestPeriod() => SIMCONNECT_PERIOD.SECOND;
-
-        public virtual void OnPress(ExtendedSimConnect sc) => sc.RequestDataOnSimObject(this, SIMCONNECT_CLIENT_DATA_PERIOD.ONCE);
-        public override void Process(ExtendedSimConnect simConnect, EngineControlSelectData data)
-        {
-            var bits = $"bits {data.bitFlags}";
-            var fsvs = $"Fuel system valves {data.fsvs1} {data.fsvs2} {data.fsvs3} {data.fsvs4}";
-            // By default MSFS sets the correct value based on number of engines.
-            //hub.Clients.All.Speak(bits);
-            Console.WriteLine(bits);
-            //hub.Clients.All.Speak(fsvs);
-            Console.WriteLine(fsvs);
-        }
-    }
-
     // UI: SET ENGINE n FUEL VALVE
     [Component] public class FuelSystemValveCloseEvent : IEvent { public string SimEvent() => "FUELSYSTEM_VALVE_CLOSE"; }
     [Component] public class FuelSystemValveOpenEvent : IEvent { public string SimEvent() => "FUELSYSTEM_VALVE_OPEN"; }
-//TODO: look at ENGINE CONTROL SELECT instead of per-engine events...
-//[Component] public class EngineMaster1SetEvent : IEvent { public string SimEvent() => "ENGINE_MASTER_1_SET"; }
-    // UI: SET ENGINE MASTER n
-//    [Component] public class EngineMasterSetEvent : IEvent { public string SimEvent() => "ENGINE_MASTER_SET"; }
-//[Component] public class Starter1SetEvent : IEvent { public string SimEvent() => "STARTER1_SET"; }
-//[Component] public class Starter2SetEvent : IEvent { public string SimEvent() => "STARTER2_SET"; }
-// Get errors back from the generic one - may not be needed with FBW...
-    // UI: SET STARTER n
-    [Component] public class StarterSetEvent : IEvent { public string SimEvent() => "STARTER_SET"; }
 
     [Component, RequiredArgsConstructor]
     public partial class EnginerMasterAction
@@ -89,20 +40,22 @@ namespace Controlzmo.Systems.Controls.Engine
             var is4engined = sc.IsA380X || sc.IsB748 || sc.IsIni400M;
             var first = isLeft ? 1u : (is4engined ? 3u : 2u);
             var last = first + (is4engined ? 1u : 0u);
-/*TODO: seems to have stopped working for the A380X, at least in MSFS2024 :-(
-Toggling on only toggles 1/2, as if we're not 4-engined.
-Toggling off turns both off but only sort of... :-o */
-            /*var newData = new EngineControlSelectData() { bitFlags = is4engined ? (isLeft ? 3 : 12) : (isLeft ? 1 : 2) };
-Console.Error.WriteLine($"newData flags {newData.bitFlags} value {value} isLeft {isLeft}");
-            sc.SendDataOnSimObject(newData);*/
-//TODO: some VAs need time between engines in the A380, e.g. Oryx. Can we do them 2s apart or something?
+            var delay = 1;
             for (var engine = first; engine <= last; ++engine) {
-                sc.SendEventEx1(isOn ? fuelSystemValveOpen : fuelSystemValveClose, engine);
-                //sc.SendEvent(engineMasterSetEvent, value);
-                //sc.SendEvent(starterSetEvent, value);
+                DelayedEngineMasterCommand(sc, isOn ? fuelSystemValveOpen : fuelSystemValveClose, engine, delay);
+                delay += 3000;
             }
-            /*newData.bitFlags = is4engined ? 15 : 3;
-            sc.SendDataOnSimObject(newData);*/
+        }
+
+        private void DelayedEngineMasterCommand(ExtendedSimConnect sc, IEvent _event, uint engine, int delay)
+        {
+            var timer = new Timer(delay);
+            timer.AutoReset = false;
+            timer.Elapsed += (object? source, ElapsedEventArgs e) => {
+                sc.SendEventEx1(_event, engine);
+                timer.Dispose(); // Probably don't need to do this. timer == source
+            };
+            timer.Start();
         }
     }
 
