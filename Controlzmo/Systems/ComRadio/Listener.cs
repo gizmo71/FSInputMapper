519bd09a4f048f1461e0b63744c796d03d5a2779
Dave Gymer <gizmo@davegymer.org>
2025-09-26 15:57:08 +0100

    Use priority targets to simplify COM swapping again. Note: does not work in iniBuilds A21N. :-(

--- Controlzmo/Systems/ComRadio/Listener.cs
+++ Controlzmo/Systems/ComRadio/Listener.cs
@@ -14,43 +14,20 @@ namespace Controlzmo.Systems.ComRadio
         public UInt32 StandbyHz { get; }
     }
 
-    public interface IComRadio
-    {
-        public void Swap(ExtendedSimConnect simConnect);
-        public int Channel { get; }
-    }
-
     [RequiredArgsConstructor]
-    public abstract partial class ComRadioManager<T> : DataListener<T>, IComRadio, IRequestDataOnOpen where T : struct, IComRadioData
+    public abstract partial class ComRadioManager<T> : DataListener<T>, IRequestDataOnOpen where T : struct, IComRadioData
     {
         private readonly IHubContext<ControlzmoHub, IControlzmoHub> hub;
-        private readonly IEvent setActive;
-        private readonly IEvent setStandby;
-        [Property]
-        private readonly int _channel;
-        private IComRadioData current;
+        private readonly int channel;
 
         public SIMCONNECT_PERIOD GetInitialRequestPeriod() => SIMCONNECT_PERIOD.VISUAL_FRAME;
 
         public override void Process(ExtendedSimConnect simConnect, T data)
         {
-            current = data;
-            hub.Clients.All.SetFrequencyFromSim($"com{Channel}active", data.ActiveHz);
-            hub.Clients.All.SetFrequencyFromSim($"com{Channel}standby", data.StandbyHz);
-        }
-
-        public void Swap(ExtendedSimConnect sc)
-        {
-Console.Error.WriteLine($"swapping {current.ActiveHz} and {current.StandbyHz}");
-            var _new = current;
-//TODO: this just doesn't work in the Fenix - probably we can't genuinely bypass whatever its doing to steal the values. :-(
-            sc.SendEvent(setActive, _new.StandbyHz, 0, SimConnect.SIMCONNECT_GROUP_PRIORITY_DEFAULT);
-            sc.SendEvent(setStandby, _new.ActiveHz, 0, SimConnect.SIMCONNECT_GROUP_PRIORITY_DEFAULT);
+            hub.Clients.All.SetFrequencyFromSim($"com{channel}active", data.ActiveHz);
+            hub.Clients.All.SetFrequencyFromSim($"com{channel}standby", data.StandbyHz);
         }
     }
-
-    [Component]
-    public class Com1ActiveSetEvent : IEvent { public string SimEvent() => "COM_RADIO_SET_HZ"; }
 
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
     public partial struct Com1Data : IComRadioData
@@ -66,11 +43,8 @@ Console.Error.WriteLine($"swapping {current.ActiveHz} and {current.StandbyHz}");
     [Component]
     public class Com1RadioListener : ComRadioManager<Com1Data>
     {
-        public Com1RadioListener(IHubContext<ControlzmoHub, IControlzmoHub> hub, Com1ActiveSetEvent a, Com1StandbyRadioSetEvent s) : base(hub, a, s, 1) { }
+        public Com1RadioListener(IHubContext<ControlzmoHub, IControlzmoHub> hub) : base(hub, 1) { }
     }
-
-    [Component]
-    public class Com2ActiveSetEvent : IEvent { public string SimEvent() => "COM2_RADIO_SET_HZ"; }
 
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
     public partial struct Com2Data : IComRadioData
@@ -86,7 +60,7 @@ Console.Error.WriteLine($"swapping {current.ActiveHz} and {current.StandbyHz}");
     [Component]
     public class Com2RadioListener : ComRadioManager<Com2Data>
     {
-        public Com2RadioListener(IHubContext<ControlzmoHub, IControlzmoHub> hub, Com2ActiveSetEvent a, Com2StandbyRadioSetEvent s) : base(hub, a, s, 2) { }
+        public Com2RadioListener(IHubContext<ControlzmoHub, IControlzmoHub> hub) : base(hub, 2) { }
     }
 
     internal static class FrequencyExtensions
--- Controlzmo/Systems/ComRadio/Swap.cs
+++ Controlzmo/Systems/ComRadio/Swap.cs
@@ -7,23 +7,31 @@ namespace Controlzmo.Systems.ComRadio
     [RequiredArgsConstructor]
     public abstract partial class ComSwap : ISettable<object>
     {
-        private readonly IComRadio radio;
+        private readonly int channel;
+        private readonly IEvent _event;
 
-        public string GetId() => $"com{radio.Channel}swap";
+        public string GetId() => $"com{channel}swap";
         public void SetInSim(ExtendedSimConnect simConnect, object? source) {
-            radio.Swap(simConnect);
+            simConnect.SendEvent(_event, 0, 0, ExtendedSimConnect.SIMCONNECT_GROUP_PRIORITY_STANDARD);
         }
     }
 
     [Component]
+    public class Com1StandbyRadioSwapEvent : IEvent { public string SimEvent() => "COM1_RADIO_SWAP"; }
+
+    [Component]
     public class Com1Swap : ComSwap
     {
-        public Com1Swap(Com1RadioListener radio) : base(radio) { }
+        public Com1Swap(Com1StandbyRadioSwapEvent e) : base(1, e) { }
     }
+
+
+    [Component]
+    public class Com2StandbyRadioSwapEvent : IEvent { public string SimEvent() => "COM2_RADIO_SWAP"; }
 
     [Component]
     public class Com2Swap : ComSwap
     {
-        public Com2Swap(Com2RadioListener radio) : base(radio) { }
+        public Com2Swap(Com2StandbyRadioSwapEvent e) : base(2, e) { }
     }
 }
