313a659a703b761f45b2611c1c46969df1f2ad38
Gizmo <gizmo@davegymer.org>
2025-12-20 14:45:48 +0000

    Very basic print support.

--- Controlzmo/Print.cs
+++ Controlzmo/Print.cs
@@ -2,47 +2,31 @@
 using Lombok.NET;
 using Microsoft.Extensions.Logging;
 using SimConnectzmo;
-using System;
-using System.IO;
+using System.Drawing;
+using System.Drawing.Printing;
 
+// Also consider https://github.com/lukevp/ESC-POS-.NET
 namespace Controlzmo
 {
     [Component]
     [RequiredArgsConstructor]
-    public partial class Print : ISettable<string>, IDisposable
+    public partial class Print : ISettable<string>
     {
         private readonly ILogger<Print> _log;
-        private string? file;
 
         public string GetId() => "printText";
 
         public void SetInSim(ExtendedSimConnect simConnect, string? value)
         {
             _log.LogInformation("Print {}", value);
-            if (file == null)
-            {
-                file = Path.GetTempFileName() + ".Controlzmo.METAR.txt";
-            }
 
-            // https://stackoverflow.com/questions/70780652/how-to-print-a-text-file-content-in-net-using-c-sharp
-            using (StreamWriter outputFile = new StreamWriter(file))
-            {
-                outputFile.WriteLine(value);
-                outputFile.WriteLine();
-                outputFile.WriteLine(new String('-', 15));
-            }
-            using (var p = new System.Diagnostics.Process()) {
-                p.StartInfo.FileName = "notepad";
-                p.StartInfo.Arguments = $"/pt \"{file}\" \"POS58 Printer\"";
-                p.StartInfo.UseShellExecute = false;
-                p.Start();
-            }
-        }
-
-        public void Dispose()
-        {
-            if (file != null)
-                File.Delete(file);
+            var doc = new PrintDocument();
+            doc.PrinterSettings.PrinterName = "Microsoft Print to PDF"; //TODO: POS58 Printer
+            doc.PrintPage += (sender, e) => {
+                //TODO: break longer lines up?
+                e.Graphics?.DrawString(value, new Font("Arial", 8), Brushes.Black, 0, 0);
+            };
+            doc.Print();
         }
     }
 }
