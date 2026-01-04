9c8ee9002952a81432067d0fa2972901058e6a3c
Gizmo <gizmo@davegymer.org>
2026-01-04 16:48:32 +0000

    WIP: working second hub.

---
+++ Controlzmo/Hubs/TestyHub.cs
@@ -0,0 +1,41 @@
+﻿using Lombok.NET;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Controlzmo.Hubs
+{
+    public partial interface ITestyHub
+    {
+        Task ToBrowser(int a, string b);
+    }
+
+    [Component]
+    public partial class Ticker : CreateOnStartup {
+        private int count = 0;
+        private readonly IHubContext<TestyHub, ITestyHub> hub;
+
+        public Ticker(IServiceProvider sp) {
+            hub = sp.GetRequiredService<IHubContext<TestyHub, ITestyHub>>();
+            var timer = new Timer(1000);
+            timer.Elapsed += Tick;
+            timer.Start();
+        }
+
+        private void Tick(object? sender, ElapsedEventArgs args) {
+            hub.Clients.All.ToBrowser(count++, "foo");
+        }
+    }
+
+    [RequiredArgsConstructor]
+    public partial class TestyHub : Hub<ITestyHub>
+    {
+        public async Task FromBrowser(string a, int b)
+        {
+Console.Error.WriteLine($"Got message from browser with {a} and {b}");
+            await Task.CompletedTask;
+        }
+    }
+}
--- Controlzmo/react/src/Testy.tsx
+++ Controlzmo/react/src/Testy.tsx
@@ -1,6 +1,50 @@
-﻿import { useState } from 'react'
+﻿import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { useEffect, useState } from 'react'
+
+// https://stackoverflow.com/a/74603295
+function useLiveUpdates(connectionRef: HubConnection | undefined) {
+    useEffect(() => {
+        if (connectionRef) {
+            try {
+                connectionRef
+                    .start()
+                    .then(() => {
+console.log('started');
+connectionRef.send('FromBrowser', "hello", 666); // If we don't actually trigger it it doesn't seem to get created...
+                    })
+                    .catch((err) => {
+                        console.error('cannot connect', err);
+                    });
+            } catch (error) {
+                console.error('cannot setup', error);
+            }
+        }
+
+        return () => {
+            connectionRef?.stop();
+console.log('stopped');
+        };
+    }, [connectionRef]);
+};
 
 export default function Testy() {
+    const [connectionRef, setConnection] = useState<HubConnection>();
+
+    useEffect(() => {
+        const con = new HubConnectionBuilder()
+            .withUrl('/hub/testy')
+            .withAutomaticReconnect()
+            .build();
+        con.on('ToBrowser', function (a, b) {
+console.log('got message', a, b);
+            con.send('FromBrowser', b, a);
+        });
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        setConnection(con);
+    }, []);
+
+    useLiveUpdates(connectionRef);
+
     const [count, setCount] = useState(0);
     return (
         <button onClick={() => setCount((count) => count + 1)}>
