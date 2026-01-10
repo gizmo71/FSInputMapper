0d77ef695edc598dbe853be4ca99a8e0a02d859e
Gizmo <gizmo@davegymer.org>
2026-01-10 19:11:02 +0000

    Remove hard coded output interface.

--- Controlzmo/Hubs/TestyHub.cs
+++ Controlzmo/Hubs/TestyHub.cs
@@ -7,30 +7,25 @@ using System.Timers;
 
 namespace Controlzmo.Hubs
 {
-    public partial interface ITestyHub
-    {
-        Task ToBrowser(int a, string b);
-    }
-
     [Component]
     public partial class Ticker : CreateOnStartup {
         private int count = 0;
-        private readonly IHubContext<TestyHub, ITestyHub> hub;
+        private readonly IHubContext<TestyHub> hub;
 
         public Ticker(IServiceProvider sp) {
-            hub = sp.GetRequiredService<IHubContext<TestyHub, ITestyHub>>();
+            hub = sp.GetRequiredService<IHubContext<TestyHub>>();
             var timer = new Timer(1000);
             timer.Elapsed += Tick;
             timer.Start();
         }
 
         private void Tick(object? sender, ElapsedEventArgs args) {
-            hub.Clients.All.ToBrowser(count++, "foo");
+            hub.Clients.All.SendAsync("ToBrowser", count++, "foo");
         }
     }
 
     [RequiredArgsConstructor]
-    public partial class TestyHub : Hub<ITestyHub>
+    public partial class TestyHub : Hub
     {
         public async Task FromBrowser(string a, int b)
         {
