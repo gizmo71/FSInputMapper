f282c4265f0729578edb7ec78335a28735b2c9d9
Dave Gymer <gizmo@davegymer.org>
2026-01-24 09:02:46 +0000

    Reduce landing cool down timer slack to 5s.

--- Controlzmo/Systems/PilotMonitoring/Landing.cs
+++ Controlzmo/Systems/PilotMonitoring/Landing.cs
@@ -149,7 +149,7 @@ namespace Controlzmo.Systems.PilotMonitoring
         }
 
         // Until/unless we discover a better to detect/predict Pegasus v2's behaviour
-        private const int SLACK = 30;
+        private const int SLACK = 5;
 
         public override void Process(ExtendedSimConnect simConnect, EngineCooldownData data)
         {
