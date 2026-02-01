f7159d9d5bf459b560d01a162b9e49de1461cede
Dave Gymer <gizmo@davegymer.org>
2026-02-01 10:45:57 +0000

    Add WinWing output via websocket service.

---
+++ Controlzmo/GameControllers/WinWingOutputs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace Controlzmo.GameControllers
+{
+    [Component]
+    public class UrsaMinorOutputs : KeepAliveWorker
+    {
+        private WebSocket? client;
+
+        public UrsaMinorOutputs(IServiceProvider sp) : base(sp) { }
+
+        protected override void OnStart(object? sender, DoWorkEventArgs args)
+        {
+            client = new WebSocket("ws://localhost:8666/Display/Set");
+        }
+
+        protected override void OnLoop(object? sender, DoWorkEventArgs args)
+        {
+            if (!client!.IsAlive)
+                client.Connect();
+        }
+
+        protected override void OnStop(object? sender, DoWorkEventArgs args)
+        {
+            client?.Close();
+        }
+
+        public void SetTrimDisplay(int decaUnits)
+        {
+            try {
+                var encoded = $"{decaUnits / 10.0:+00.0;-00.0}";
+                Task.Run(() => client?.Send(encoded));
+            } catch (Exception ex) {
+                Console.Error.WriteLine("Failed to send trim display: {0}", ex);
+            }
+        }
+    }
+}
