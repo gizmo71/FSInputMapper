689d496441fefe5f50f698e099d46b19c1fb2335
Dave Gymer <gizmo@davegymer.org>
2026-02-21 11:25:58 +0000

    Make display of rudder trim work in A380X, A32NX, and iniBuilds A21N/A330.

--- Controlzmo/Systems/Controls/RudderTrim.cs
+++ Controlzmo/Systems/Controls/RudderTrim.cs
@@ -6,17 +6,20 @@ using Microsoft.FlightSimulator.SimConnect;
 using SimConnectzmo;
 using System;
 using System.Runtime.InteropServices;
-using static Controlzmo.GameControllers.IVJoyControllerExtensions;
 
 namespace Controlzmo.Systems.Controls
 {
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
     public struct RudderTrimData
     {
-        [SimVar("RUDDER TRIM PCT", "Percent over 100", SIMCONNECT_DATATYPE.FLOAT32, 0.00005f)]
+        [SimVar("RUDDER TRIM", "Degrees", SIMCONNECT_DATATYPE.FLOAT32, 0.00005f)]
         public float trim;
+        [SimVar("L:RUDDER_TRIM_TARGET_ANGLE", "number", SIMCONNECT_DATATYPE.FLOAT32, 0.00005f)]
+        public float iniBuilds;
+        [SimVar("L:A32NX_RUDDER_TRIM_ACTUAL_POSITION", "number", SIMCONNECT_DATATYPE.FLOAT32, 0.005f)]
+        public float a380xTrim;
         [SimVar("L:A32NX_HYD_RUDDER_TRIM_FEEDBACK_ANGLE", "number", SIMCONNECT_DATATYPE.FLOAT32, 0.005f)]
-        public float fbwTrim;
+        public float a32nxTrim;
         [SimVar("L:N_FC_RUDDER_TRIM_DECIMAL", "number", SIMCONNECT_DATATYPE.INT32, 0.5f)]
         public Int32 fenixDecaUnits;
     };
@@ -29,11 +32,15 @@ namespace Controlzmo.Systems.Controls
 
         public override void Process(ExtendedSimConnect simConnect, RudderTrimData data)
         {
-Console.Error.WriteLine($"trim percent {data.fenixDecaUnits} FBW {data.fbwTrim} generic {data.trim}");
-            if (simConnect.IsFBW)
-                data.fenixDecaUnits = (Int32) (Math.Round(data.fbwTrim * 10.0, MidpointRounding.AwayFromZero));
+Console.Error.WriteLine($"trim percent {data.fenixDecaUnits} FBW {data.a32nxTrim} ini {data.iniBuilds} generic {data.trim}");
+            if (simConnect.IsA380X)
+                data.fenixDecaUnits = (Int32) (Math.Round(data.a380xTrim * -10.0, MidpointRounding.AwayFromZero));
+            else if (simConnect.IsA32NX)
+                data.fenixDecaUnits = (Int32) (Math.Round(data.a32nxTrim * 10.0, MidpointRounding.AwayFromZero));
+            else if (simConnect.IsIniBuilds)
+                data.fenixDecaUnits = (Int32) (Math.Round(data.iniBuilds * 10.0, MidpointRounding.ToZero));
             else if (!simConnect.IsFenix)
-                data.fenixDecaUnits = (Int32) (data.trim * 200.0);
+                data.fenixDecaUnits = (Int32) (data.trim * 2.0);
 Console.Error.WriteLine($"             -> {data.fenixDecaUnits}");
             output.SetTrimDisplay(data.fenixDecaUnits);
         }
@@ -71,6 +78,7 @@ Console.Error.WriteLine($"             -> {data.fenixDecaUnits}");
                 _sender.Execute(sc, $"{value} (>L:S_FC_RUDDER_TRIM)");
             else {
                 var controller = vJoy.getController();
+Console.WriteLine($"Sending RT for {value}");
                 PressOrRelease(value == LEFT)(controller, VJoyButton.RUDDER_TRIM_LEFT);
                 PressOrRelease(value == RIGHT)(controller, VJoyButton.RUDDER_TRIM_RIGHT);
             }
