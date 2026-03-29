79cca2ced00ae1ac1fdf3c9a790aa92a22440cee
Dave Gymer <gizmo@davegymer.org>
2026-03-29 11:50:31 +0100

    Add a means to quickly remove the yoke and microphones in the ATR.

---
+++ Controlzmo/Systems/Atc/AircraftSetup.cs
@@ -0,0 +1,24 @@
+﻿using Controlzmo.Hubs;
+using Controlzmo.Systems.JetBridge;
+using Lombok.NET;
+using SimConnectzmo;
+
+namespace Controlzmo.Systems.Atc
+{
+    [Component, RequiredArgsConstructor]
+    public partial class AircraftSetup : ISettable<string?>
+    {
+        private readonly JetBridgeSender sender;
+
+        public string GetId() => "initAircraftState";
+
+        public void SetInSim(ExtendedSimConnect simConnect, string? value)
+        {
+            if (simConnect.IsAtr7x)
+            {
+                sender.Execute(simConnect, "1 (>L:MSATR_MICROPHONE_LEFT_HIDDEN) 1 (>L:MSATR_MICROPHONE_RIGHT_HIDDEN)");
+                sender.Execute(simConnect, "1 (>L:XMLVAR_YOKEHIDDEN1) 1 (>L:XMLVAR_YOKEHIDDEN2)");
+            }
+        }
+    }
+}
