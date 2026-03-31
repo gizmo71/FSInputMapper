d643fcf8bd3d9ce09d2d88512e90c54e9aacf004
Dave Gymer <gizmo@davegymer.org>
2026-03-31 17:58:34 +0100

    Support ATR Chrono

--- Controlzmo/Systems/EfisControlPanel/Chrono.cs
+++ Controlzmo/Systems/EfisControlPanel/Chrono.cs
@@ -9,25 +9,29 @@ namespace Controlzmo.Systems.EfisControlPanel
     [Component]
     public class Chrono1Event : IEvent { public string SimEvent() => "A32NX.EFIS_L_CHRONO_PUSHED"; }
 
-    [Component]
-    [RequiredArgsConstructor]
-    public partial class ChronoButton : ISettable<object>
+    [Component, RequiredArgsConstructor]
+    public partial class ChronoButton : ISettable<bool>
     {
         private readonly JetBridgeSender sender;
         private readonly Chrono1Event e;
 
         public string GetId() => "chrono1press";
 
-        public void SetInSim(ExtendedSimConnect simConnect, object? value)
+        public void PressAndRelease(ExtendedSimConnect simConnect)
+        {
+            SetInSim(simConnect, true);
+            SetInSim(simConnect, false);
+        }
+
+        public void SetInSim(ExtendedSimConnect simConnect, bool isPressed)
         {
             if (simConnect.IsFenix)
-            {
-                for (var i = 1; i >= 0; --i)
-                    sender.Execute(simConnect, $"{i} (>L:S_MIP_CHRONO_CAPT)");
-            }
-            else if (simConnect.IsIniBuilds)
+                sender.Execute(simConnect, $"{(isPressed ? 1 : 0)} (>L:S_MIP_CHRONO_CAPT)");
+            else if (simConnect.IsIniBuilds && isPressed)
                 sender.Execute(simConnect, $"1 (>L:INI_CPT_CHRONO_BUTTON)");
-            else if (simConnect.IsFBW)
+            else if (simConnect.IsAtr7x && isPressed)
+                sender.Execute(simConnect, $"1 (>L:MSATR_CLCK_CHRONO_1)");
+            else if (simConnect.IsFBW && isPressed)
                 simConnect.SendEvent(e);
         }
     }
