d21f6133b18526bdc307538f992edadabdb1f42d
Dave Gymer <gizmo@davegymer.org>
2026-04-05 12:08:48 +0100

    Autopilot on/off in ATR needs to take YD into account

--- Controlzmo/Systems/FlightControlUnit/Autopilot.cs
+++ Controlzmo/Systems/FlightControlUnit/Autopilot.cs
@@ -20,6 +20,8 @@ namespace Controlzmo.Systems.FlightControlUnit
                 sender.Execute(sc, "(L:A32NX_AUTOPILOT_1_ACTIVE) if{ (>K:A32NX.FCU_AP_2_PUSH) } els{ (>K:A32NX.FCU_AP_1_PUSH) }");
             else if (sc.IsIni330 || sc.IsIni321)
                 sender.Execute(sc, "1 (L:INI_ap1_on) if{ (>L:INI_AP2_BUTTON) } els{ (>L:INI_AP1_BUTTON) }");
+            else if (sc.IsAtr7x)
+                sender.Execute(sc, "1 (L:MSATR_FGCP_STAT_AP) - (>L:MSATR_FGCP_AP)"); // Also turns on YD
             else
                 sc.SendEvent(_event);
         }
@@ -31,7 +33,14 @@ namespace Controlzmo.Systems.FlightControlUnit
     public partial class AutopilotOff : IButtonCallback<UrsaMinorFighterR>
     {
         private readonly AutopilotOffEvent _event;
+        private readonly JetBridgeSender sender;
         public int GetButton() => UrsaMinorFighterR.BUTTON_RED;
-        public void OnPress(ExtendedSimConnect sc) => sc.SendEvent(_event);
+
+        public void OnPress(ExtendedSimConnect sc)
+        {
+            if (sc.IsAtr7x)
+                sender.Execute(sc, "(L:MSATR_FGCP_STAT_YD) (>L:MSATR_FGCP_YD)"); // Make sure YD/AP are sync'd
+            sc.SendEvent(_event);
+        }
     }
 }
