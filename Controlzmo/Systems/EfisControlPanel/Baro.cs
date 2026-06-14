eaeffaaee2f061115fefd50568bf334cd0dddd75
Dave Gymer <gizmo@davegymer.org>
2026-06-14 07:36:57 +0100

    Make A380 push/pull work the same as the older Airbuses.

--- Controlzmo/Systems/EfisControlPanel/Baro.cs
+++ Controlzmo/Systems/EfisControlPanel/Baro.cs
@@ -166,6 +166,7 @@ System.Console.WriteLine($"-> {value} led to {command}");
             {
                 var command = @"(L:XMLVAR_Baro1_Mode) 2 & 0 != if{ 2 } els{ 1 } (L:XMLVAR_Baro1_Mode) ^ (>L:XMLVAR_Baro1_Mode)";
                 if (sc.IsFenix) command = "(L:S_FCU_EFIS1_BARO_STD) -- (>L:S_FCU_EFIS1_BARO_STD)";
+                else if (sc.IsA380X) command = "(>H:A380X_EFIS_CP_BARO_PULL_1)"; // Yes, Pull
                 else if (sc.IsA32NX || sc!.IsA339) command = "(>K:A32NX.FCU_EFIS_L_BARO_PUSH)";
                 else if (sc.IsIniBuilds) command = @"1 (>L:INI_1_ALTIMETER_PUSH_COMMAND)";
                 sender.Execute(sc, command);
@@ -222,6 +223,7 @@ System.Console.WriteLine($"-> {value} led to {command}");
         {
             var command = @"(L:XMLVAR_Baro1_Mode) 2 | (>L:XMLVAR_Baro1_Mode)";
             if (simConnect.IsFenix) command = @"(L:S_FCU_EFIS1_BARO_STD) ++ (>L:S_FCU_EFIS1_BARO_STD)";
+            else if (simConnect.IsA380X) command = "(>H:A380X_EFIS_CP_BARO_PUSH_1)"; // Yes, Push!
             else if (simConnect.IsA32NX || simConnect.IsA339) command = "(>K:A32NX.FCU_EFIS_L_BARO_PULL)";
             else if (simConnect.IsIniBuilds) command = @"1 (>L:INI_1_ALTIMETER_PULL_COMMAND)";
             else if (simConnect.IsAtr) command = @"1 (>L:MSATR_BARO_STD_1)";
