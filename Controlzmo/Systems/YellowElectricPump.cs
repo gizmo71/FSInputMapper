b447b4d738beb58c3804b21866ae7ca6961be670
Dave Gymer <gizmo@davegymer.org>
2026-08-09 09:51:38 +0100

    Add A32NX support for yellow electric pump button

--- Controlzmo/Systems/YellowElectricPump.cs
+++ Controlzmo/Systems/YellowElectricPump.cs
@@ -12,6 +12,10 @@ namespace Controlzmo.Systems
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
     public struct YellowElectricPumpData
     {
+        [SimVar("L:A32NX_OVHD_HYD_EPUMPY_PB_IS_AUTO", "bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
+        public Int32 isAutoA32nx;
+        [SimVar("L:A32NX_OVHD_HYD_EPUMPY_PB_HAS_FAULT", "bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
+        public Int32 isFaultA32nx;
         [SimVar("L:INI_ENG2_YELLOW_ELEC_PUMP_PB", "bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
         public Int32 isOnIni;
         [SimVar("L:I_OH_HYD_YELLOW_ELEC_PUMP_U", "bool", SIMCONNECT_DATATYPE.INT32, 0.5f)]
@@ -34,6 +38,7 @@ namespace Controlzmo.Systems
             var isOn = false;
             if (simConnect.IsIniBuilds) isOn = data.isOnIni == 1;
             else if (simConnect.IsFenix) { isOn = data.isOnFenix != 0; isFault = data.isFaultFenix != 0; }
+            else if (simConnect.IsA32NX) { isOn = data.isAutoA32nx == 0; isFault = data.isFaultA32nx != 0;}
             string colour = "";
             if (isFault) colour = "red";
             else if (isOn) colour = "white";
@@ -45,6 +50,8 @@ namespace Controlzmo.Systems
                 sender.Execute(simConnect, "(L:INI_ENG2_YELLOW_ELEC_PUMP_PB) ! (>L:INI_ENG2_YELLOW_ELEC_PUMP_PB)");
             else if (simConnect.IsFenix)
                 sender.Execute(simConnect, "(L:S_OH_HYD_YELLOW_ELEC_PUMP) ++ (>L:S_OH_HYD_YELLOW_ELEC_PUMP)");
+            else if (simConnect.IsA32NX && isPressed)
+                sender.Execute(simConnect, "(L:A32NX_OVHD_HYD_EPUMPY_PB_IS_AUTO, Bool) ! (>L:A32NX_OVHD_HYD_EPUMPY_PB_IS_AUTO)");
         }
     }
 }
